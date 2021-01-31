80e4aba26084da37ab5cab2d36242ac22d567925
noidname <tim983649@gmail.com>
2021-01-31 17:21:37 +0800

    slidebar wip

--- src/client/features/editor/index.js
+++ src/client/features/editor/index.js
@@ -9,23 +9,36 @@ import SlideBar from "./slidebar";
 
 const Editor = (props) => {
   // ========= State need to be change into Redux ==============
+
   const [currentTime, setCurrentTime] = useState(0);
-  const [currentMode, setCurrentMode] = useState("");
+  const [currentMode, setCurrentMode] = useState("SAVE");
   const [currentFrame, setCurrentFrame] = useState(0);
   const [currentPeopleNum, setCurrentPeopleNum] = useState(8);
+
+  const [currentChoose, setCurrentChoose] = useState(
+    Array(currentPeopleNum).fill(false)
+  );
+
   // ========= State need to be change into Redux ==============
 
-  const renderRatioButtons = () => {
+  const renderCheckBoxes = () => {
     let ratioButtonList = [];
     for (let i = 0; i < currentPeopleNum; i++) {
       ratioButtonList.push(
         <div className="form-check form-check-inline">
           <input
             className="form-check-input"
-            type="radio"
-            name="inlineRadioOptions"
-            id={`people_ratio_${i}`}
+            type="checkbox"
+            id={`people_chekbox_${i}`}
             value="option1"
+            onChange={() => {
+              setCurrentChoose((state) =>
+                state.map((choose, j) => {
+                  return i === j ? !choose : choose;
+                })
+              );
+            }}
+            checked={currentChoose[i]}
           />
           <label className="form-check-label" for={`people_ratio_${i}`}>
             {i}
@@ -39,7 +52,14 @@ const Editor = (props) => {
 
   const renderSlideBars = () => {
     return LIGHTPARTS.map((lightpart) => {
-      return <SlideBar partName={lightpart} />;
+      return (
+        <SlideBar
+          partName={lightpart}
+          disabled={
+            currentMode === "EDIT" || currentMode === "ADD" ? false : true
+          }
+        />
+      );
     });
   };
 
@@ -54,33 +74,107 @@ const Editor = (props) => {
           type="text"
           className="form-control"
           aria-describedby="basic-addon1"
+          value={currentTime}
+          onChange={(e) => {
+            setCurrentTime(e.target.value ? parseInt(e.target.value, 10) : "");
+          }}
         />
         {/* =============== Time  input ================ */}
         {/* ============== Frame Control ============== */}
         <div className="btn-group" role="group">
-          <button type="button" className="btn btn-primary me-2">
+          <button
+            type="button"
+            className="btn btn-primary me-2"
+            disabled={currentFrame === 0 ? true : false}
+            onClick={(e) => {
+              e.preventDefault();
+              setCurrentFrame((state) => state - 1);
+            }}
+          >
             {"<"}
           </button>
-          <input type="number" className="form-control me-2" />
-          <button type="button" className="btn btn-primary">
+          <input
+            type="number"
+            className="form-control me-2"
+            placeholder="Frame"
+            onChange={(e) => {
+              setCurrentFrame(
+                e.target.value ? parseInt(e.target.value, 10) : ""
+              );
+            }}
+            value={currentFrame}
+            min="0"
+          />
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={(e) => {
+              e.preventDefault();
+              setCurrentFrame((state) => state + 1);
+            }}
+          >
             {">"}
           </button>
         </div>
         {/* ============== Frame Control ============== */}
       </div>
       {/* =============== People Ratio =============== */}
-      <div>{renderRatioButtons()}</div>
+      <div className="input-group" role="group">
+        {renderCheckBoxes()}
+        <button
+          className="btn btn-primary"
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            setCurrentChoose(Array(currentPeopleNum).fill(true));
+          }}
+        >
+          Choose All
+        </button>
+
+        <button
+          className="btn btn-primary"
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            setCurrentChoose(Array(currentPeopleNum).fill(false));
+          }}
+        >
+          Cancel All
+        </button>
+      </div>
       {/* =============== People Ratio =============== */}
 
       {/* =============== Mode Select =============== */}
       <div className="btn-group" role="group" aria-label="Basic example">
-        <button type="button" className="btn btn-primary me-2">
+        <button
+          type="button"
+          className="btn btn-primary me-2"
+          onClick={(e) => {
+            e.preventDefault();
+            setCurrentMode("EDIT");
+          }}
+        >
           EDIT
         </button>
-        <button type="button" className="btn btn-primary me-2">
+        <button
+          type="button"
+          className="btn btn-primary me-2"
+          onClick={(e) => {
+            e.preventDefault();
+            setCurrentMode("ADD");
+          }}
+        >
           ADD
         </button>
-        <button type="button" className="btn btn-primary">
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={(e) => {
+            e.preventDefault();
+            setCurrentMode("SAVE");
+          }}
+        >
           SAVE
         </button>
       </div>
--- src/client/features/editor/slidebar.js
+++ src/client/features/editor/slidebar.js
@@ -1,37 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SliderBar = (props) => {
-  const { partName } = props;
+  const { partName, disabled } = props;
+
+  const [value, setValue] = useState(0);
+
+  const step = 1;
+
+  const keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+  const preventDefault = (e) => e.preventDefault();
+  const preventDefaultForScrollKeys = (e) => {
+    if (keys[e.keyCode]) {
+      preventDefault(e);
+      return false;
+    }
+  };
+  const mousewheelevt = /Firefox/i.test(navigator.userAgent)
+    ? "DOMMouseScroll"
+    : "mousewheel";
+
+  // modern Chrome requires { passive: false } when adding event
+  const supportsPassive = true;
+
+  const disableWindowScroll = () => {
+    const wheelOpt = supportsPassive ? { passive: false } : false;
+    window.addEventListener(mousewheelevt, preventDefault, wheelOpt); // modern desktop
+    /* window.addEventListener("touchmove", preventDefault, wheelOpt); // mobile
+    window.addEventListener("keydown", preventDefaultForScrollKeys, false); */
+  };
+
+  const enableWindowScroll = (e) => {
+    const wheelOpt = supportsPassive ? { passive: false } : false;
+    console.log("enableWindowScroll");
+    window.removeEventListener(mousewheelevt, preventDefault, wheelOpt);
+    /* window.removeEventListener("touchmove", preventDefault, wheelOpt);
+    window.removeEventListener("keydown", preventDefaultForScrollKeys, false); */
+  };
 
   const moveSlider = (e) => {
     // console.log("moveSlider");
-    const zoomLevel = parseInt(e.target.value);
+    const zoomLevel = parseInt(e.target.value, 10);
 
-    if (e.originalEvent.wheelDelta < 0) {
-      //scroll down
-      e.target.value = zoomLevel - 10;
+    // console.log(e.deltaY);
+
+    // ========================= disable window scrolling ==================
+    disableWindowScroll();
+    // ========================= disable window scrolling ==================
+
+    if (e.deltaY < 0) {
+      //  scroll down
+      e.target.value = zoomLevel - step;
+    } else if (e.deltaY > 0) {
+      //  scroll up
+      e.target.value = zoomLevel + step;
     } else {
-      //scroll up
-      e.target.value = zoomLevel + 10;
+      e.target.value = zoomLevel;
     }
 
-    e.target.change();
-
-    //prevent page fom scrolling
-    return false;
+    setValue(e.target.value);
   };
 
   const handleWheel = (e) => {
-    console.log("onMouseOver");
-    const mousewheelevt = /Firefox/i.test(navigator.userAgent)
-      ? "DOMMouseScroll"
-      : "mousewheel";
-    // console.log(e.deltaY);
     e.target.addEventListener(mousewheelevt, moveSlider);
   };
 
   const handleChange = (e) => {
-    //TODO
+    setValue(e.target.value);
+  };
+
+  const handleInputChange = (e) => {
+    setValue(e.target.value);
   };
 
   return (
@@ -45,10 +83,20 @@ const SliderBar = (props) => {
           className="form-range form-control me-2"
           style={{ border: "none" }}
           id={partName}
+          value={value}
           onMouseOver={handleWheel}
+          onMouseOut={enableWindowScroll}
           onChange={handleChange}
+          disabled={disabled}
         />
-        <input type="number" className="form-control" />
+        <input
+          type="number"
+          className="form-control"
+          value={value / 100}
+          onChange={handleInputChange}
+          disabled={disabled}
+          min="0"
+        />
       </div>
     </>
   );
