775a1126a04fcb60ba8e9a045268123fe9dfc0c0
Kenchu123 <k88913n@gmail.com>
2021-03-03 14:09:53 +0800

    set docker prod

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:14.15.5
 WORKDIR /app
-
+ENV NODE_ENV=prod
 COPY ["package.json", "package-lock.json*", "./"]
 RUN npm install
 COPY . .
--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,11 +1,11 @@
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable global-require */
-require("dotenv").config();
 const { merge } = require("webpack-merge");
 
 const commonConfig = require("./webpack.common.js");
 
 const env = process.env.NODE_ENV || "dev";
+console.log(`config = ./webpack.${env}.js`);
 
 const envConfig = require(`./webpack.${env}.js`);
 
