555408c8b80b3541309ed628be456da16a889f8c
Mason Chen <b08901058@ntu.edu.tw>
2021-03-04 00:17:48 +0800

    input bug fixed

--- src/client/components/login/index.js
+++ src/client/components/login/index.js
@@ -12,8 +12,8 @@ import { login } from "../../slices/globalSlice";
 import store from "../../store";
 
 export default function Login() {
-  const [username, setUsername] = useState();
-  const [password, setPassword] = useState();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [open, setOpen] = useState(false);
 
   const handleClose = () => {
