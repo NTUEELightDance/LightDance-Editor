e392fc2af7921e87ef37e88b8d2a0f29a979d1c4
Kenchu123 <k88913n@gmail.com>
2021-03-28 20:00:32 +0800

    merge

--- utils/merge.js
+++ utils/merge.js
@@ -38,7 +38,58 @@ try {
 }
 const json2 = JSON.parse(raw);
 
+function compareNumbers(a, b) {
+  return a.start - b.start;
+}
+
+// check json1 increasing
+json1.sort(compareNumbers);
+for (let i = 0; i < json1.length - 1; ++i) {
+  if (json1[i].start > json1[i + 1].start) {
+    console.error(`[Error] ${json_1} at ${i} is not an increasing format`);
+    exit();
+  }
+}
+
+// check json2 increasing
+json2.sort(compareNumbers);
+for (let i = 0; i < json2.length - 1; ++i) {
+  if (json2[i].start > json2[i + 1].start) {
+    console.error(`[Error] ${json_2} at ${i} is not an increasing format`);
+    exit();
+  }
+}
+
+// merge
+const new_json = [];
+let i = 0,
+  j = 0;
+for (let k = 0; k < json1.length + json2.length; ++k) {
+  if (i >= json1.length || j >= json2.length) break;
+  if (json1[i].start >= json2[j].start) {
+    new_json.push(json2[j]);
+    j += 1;
+  } else if (json1[i].start < json2[j].start) {
+    new_json.push(json1[i]);
+    i += 1;
+  }
+}
+
+if (i === json1.length) {
+  for (j; j < json2.length; ++j) new_json.push(json2[j]);
+} else if (j === json2.length) {
+  for (i; i < json1.length; ++i) new_json.push(json1[j]);
+}
+
+// check new json increasing
+for (let i = 0; i < new_json.length - 1; ++i) {
+  if (new_json[i].start > new_json[i + 1].start) {
+    console.error(`[Error] ${new_json} at ${i} is not an increasing format`);
+    exit();
+  }
+}
+
 // Write File
-fs.writeFile(outputPath, JSON.stringify([...json1, ...json2]), () => {
+fs.writeFile(outputPath, JSON.stringify(new_json), () => {
   console.log(`Writing new file to ... ${outputPath}`);
 });
