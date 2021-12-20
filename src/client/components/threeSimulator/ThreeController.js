1b6473ff476d47113361831c4b44c6e5925cec90
Mason Chen <b08901058@ntu.edu.tw>
2021-12-20 21:56:33 +0800

    add sword to the scene

--- src/client/components/threeSimulator/ThreeController.js
+++ src/client/components/threeSimulator/ThreeController.js
@@ -1,6 +1,14 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
+import Stats from "three/examples/jsm/libs/stats.module";
+import { GPUStatsPanel } from "three/examples/jsm/utils/GPUStatsPanel";
+
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
+import { SMAAPass } from "three/examples/jsm/postprocessing/SMAAPass";
+import { GUI } from "three/examples/jsm/libs/lil-gui.module.min";
+
 import { setItem, getItem } from "../../utils/localStorage";
 // redux actions and store
 import { posInit, controlInit } from "../../slices/globalSlice";
@@ -8,6 +16,7 @@ import store from "../../store";
 // components
 
 import ThreeDancer from "./threeComponents/Dancer";
+import ThreeSword from "./threeComponents/Sword";
 
 import {
   updateFrameByTime,
@@ -15,9 +24,14 @@ import {
   fadeStatus,
 } from "../../utils/math";
 
-const fov = 75;
-const aspect = 2; // the canvas default
-const near = 0.1;
+// const fov = 75;
+// const aspect = 2; // the canvas default
+// const near = 0.1;
+// const far = 100;
+
+const fov = 45;
+const aspect = window.innerWidth / window.innerHeight;
+const near = 0.2;
 const far = 100;
 
 /**
@@ -59,19 +73,32 @@ class ThreeController {
       AA = false;
     }
     const renderer = new THREE.WebGLRenderer({
-      // antialias: true,
       antialias: AA,
       powerPreference: "high-performance",
     });
     renderer.setSize(this.width, this.height);
-    renderer.setPixelRatio(window.devicePixelRatio / 1.3);
+    renderer.setPixelRatio(window.devicePixelRatio);
     renderer.outputEncoding = THREE.sRGBEncoding;
 
     this.renderer = renderer;
 
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(0, 0, 10);
-
+    camera.position.set(
+      -0.27423481610277156,
+      3.9713106563331033,
+      17.22495526821853
+    );
+    camera.quaternion.set(
+      0.9990621561475012,
+      -0.04140624566400079,
+      0.012651325861071754,
+      0.0005243356515463121
+    );
+    camera.rotation.set(
+      -0.08286926974888807,
+      0.025238179467967838,
+      0.0020960446794741658
+    );
     camera.aspect = this.width / this.height;
     camera.updateProjectionMatrix();
 
@@ -81,21 +108,41 @@ class ThreeController {
     controls.enablePan = true;
     controls.enableZoom = true;
     // controls.screenSpacePanning = true;
-    controls.target.set(0, 1, 0);
+    controls.target.set(
+      -0.7125719340319995,
+      2.533987823530335,
+      -0.07978443261089622
+    );
+    controls.position0.set(
+      -4.4559744063642555,
+      2.128295812145451,
+      16.22834309576409
+    );
+
     controls.update();
 
     this.controls = controls;
 
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x000000);
-    // const light = new THREE.HemisphereLight(0xffffbb, 0x080820, 3);
-    // scene.add(light);
+    scene.background = new THREE.Color(0x00000);
 
-    const grid = new THREE.GridHelper(50, 50, 0x888888, 0x444444);
+    const light = new THREE.HemisphereLight(0xffffbb, 0x080820, 3);
+    scene.add(light);
 
+    const grid = new THREE.GridHelper(25, 25, 0x888888, 0x444444);
     scene.add(grid);
 
     this.scene = scene;
+
+    const composer = new EffectComposer(renderer);
+    composer.addPass(new RenderPass(scene, camera));
+
+    const pass = new SMAAPass(
+      window.innerWidth * renderer.getPixelRatio(),
+      window.innerHeight * renderer.getPixelRatio()
+    );
+    composer.addPass(pass);
+    this.composer = composer;
 
     this.clock = new THREE.Clock();
 
@@ -109,9 +156,9 @@ class ThreeController {
       if (!name.includes("sw")) {
         const newDancer = new ThreeDancer(this.scene, name);
         const newPos = {
-          x: position.x / 30,
+          x: position.x / 35,
           y: 0,
-          z: position.z / 30,
+          z: position.z / 35,
         };
         newDancer.addModel2Scene(newPos);
         this.dancers[name] = newDancer;
@@ -119,11 +166,43 @@ class ThreeController {
       }
     });
 
+    const newSword = new ThreeSword(this.scene, "sword");
+    const newPos = {
+      x: 0,
+      y: 0,
+      z: 0,
+    };
+    newSword.addModel2Scene(newPos);
+
+    const params = {
+      color: 0x000000,
+    };
+
+    const gui = new GUI();
+    gui.addColor(params, "color").onChange(function (value) {
+      scene.background.set(value);
+    });
+
     this.animateID = requestAnimationFrame(() =>
       this.animate((clockDelta) => {
         return;
       })
     );
+
+    // Add stats to monitor fps
+    const stats = new Stats();
+    document.body.appendChild(stats.domElement);
+
+    // const gpuPanel = new GPUStatsPanel(renderer.getContext());
+    // stats.addPanel(gpuPanel);
+    // stats.showPanel(0);
+    // this.stats = stats;
+    // this.gpuPanel = gpuPanel;
+
+    requestAnimationFrame(function loop() {
+      stats.update();
+      requestAnimationFrame(loop);
+    });
   }
 
   update(clockDelta) {
@@ -182,7 +261,10 @@ class ThreeController {
   }
 
   animate(animation) {
+    // this.gpuPanel.startQuery();
     this.renderer.render(this.scene, this.camera);
+    // this.gpuPanel.endQuery();
+
     if (this.isPlaying) {
       this.update(this.clock.getDelta());
     } else {
---
+++ src/client/components/threeSimulator/threeComponents/Sword.js
@@ -0,0 +1,95 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+class ThreeSword {
+  constructor(scene, name) {
+    this.scene = scene;
+    this.model = null;
+    this.skeleton = null;
+    this.mixer = null;
+    this.name = name;
+    this.parts = {};
+  }
+
+  addModel2Scene(position) {
+    const loader = new GLTFLoader();
+    const initModel = (gltf) => {
+      this.model = gltf.scene;
+      this.model.scale.set(0.1, 0.1, 0.1);
+      this.model.rotation.set(3.14, 0, 0);
+
+      console.log(this.model);
+
+      // const guardMesh = this.model.getObjectByName("mesh_0");
+      // // console.log(guardMesh);
+      // guardMesh.material.color.setHex(0x000000);
+      // guardMesh.material.emissive.setHex(0x284895);
+      // guardMesh.material.emissiveIntensity = 0.7;
+      // this.parts.GUARD = guardMesh;
+      // const hairMesh = this.model.getObjectByName("Hair");
+      // hairMesh.material.color.setHex(0x000000);
+      // hairMesh.material.emissive.setHex(0xcc00ff);
+      // hairMesh.material.emissiveIntensity = 0.0;
+      // this.parts.S_HAT = hairMesh;
+
+      // const bodyMesh = this.model.getObjectByName("Body");
+      // bodyMesh.material.color.setHex(0x000000);
+      // bodyMesh.material.emissive.setHex(0x004cff);
+      // bodyMesh.material.emissiveIntensity = 0.0;
+      // this.parts.S_L_HAND = bodyMesh;
+
+      // const bottomsMesh = this.model.getObjectByName("Bottoms");
+      // bottomsMesh.material.color.setHex(0x000000);
+      // bottomsMesh.material.emissive.setHex(0x004cff);
+      // bottomsMesh.material.emissiveIntensity = 0.0;
+      // this.parts.S_R_PANT = bottomsMesh;
+
+      // const shoesMesh = this.model.getObjectByName("Shoes");
+      // shoesMesh.material.color.setHex(0x000000);
+      // shoesMesh.material.emissive.setHex(0xff0000);
+      // shoesMesh.material.emissiveIntensity = 0.0;
+      // this.parts.S_L_SHOE = shoesMesh;
+
+      // const topsMesh = this.model.getObjectByName("Tops");
+      // topsMesh.material.color.setHex(0x000000);
+      // topsMesh.material.emissive.setHex(0x8000ff);
+      // topsMesh.material.emissiveIntensity = 0.0;
+      // this.parts.S_L_COAT = topsMesh;
+
+      this.skeleton = new THREE.SkeletonHelper(this.model);
+      this.skeleton.visible = false;
+
+      this.scene.add(this.model);
+      this.scene.add(this.skeleton);
+      this.model.position.setX(position.x);
+      this.model.position.setY(position.y);
+      this.model.position.setZ(position.z);
+    };
+    loader.load("data/models/sword.gltf", initModel.bind(this));
+  }
+
+  update(currentPos, currentStatus) {
+    this.updatePos(currentPos);
+    this.updateStatus(currentStatus);
+  }
+
+  updatePos(currentPos) {
+    this.model.position.setX(currentPos.x / 35);
+    this.model.position.setY(0);
+    this.model.position.setZ(currentPos.z / 35);
+  }
+
+  updateStatus(currentStatus) {
+    Object.entries(this.parts).forEach(([name, e]) => {
+      e.material.emissiveIntensity = currentStatus[name];
+    });
+  }
+
+  updateColor(color) {
+    Object.values(this.parts).forEach(([name, e]) => {
+      e.material.color.setHex(color);
+    });
+  }
+}
+
+export default ThreeSword;
