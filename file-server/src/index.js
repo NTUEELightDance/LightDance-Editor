10d833021c30116261f131e7de08812461c12b59
Kenchu123 <k88913n@gmail.com>
2022-01-21 16:24:44 +0800

    change files path

--- file-server/src/index.js
+++ file-server/src/index.js
@@ -3,11 +3,11 @@ const path = require("path");
 
 const app = express();
 
-const assetPath = path.resolve(__dirname, "..", "..", "./asset");
+const assetPath = path.resolve(__dirname, "..", "..", "files", "asset");
 app.use("/asset", express.static(assetPath));
-const musicPath = path.resolve(__dirname, "..", "..", "./music");
+const musicPath = path.resolve(__dirname, "..", "..", "files", "music");
 app.use("/music", express.static(musicPath));
-const dataPath = path.resolve(__dirname, "..", "..", "./data");
+const dataPath = path.resolve(__dirname, "..", "..", "files", "data");
 app.use("/data", express.static(dataPath));
 
 const port = process.env.PORT || 8081;
