b060c09e78007c883abc1d4f62dc9519d6956d63
zhuang-jia-xu <76544194+zhuang-jia-xu@users.noreply.github.com>
2022-01-30 01:53:09 +0800

    update preference layout

--- editor/components/Settings/Preference/index.tsx
+++ editor/components/Settings/Preference/index.tsx
@@ -1,29 +1,32 @@
-import { Select, MenuItem, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import { useContext } from "react";
 import { LayoutContext } from "../../../contexts/LayoutContext";
 import { layoutContext, editorPreference } from "../../../types/layout";
+import { PreferenceSelector } from "./PreferenceSelector";
 
 const Preference = () => {
   const { preferedEditor, setPreferedEditor } = useContext(
     LayoutContext
   ) as layoutContext;
-  const editorPreferences = ["default", "legacy", "mirrored"];
   return (
-    <>
-      <Select
+    <Box sx={{ display: "flex", flexDirection: "column", gap: "1em" }}>
+      <PreferenceSelector
+        label="prefered editor"
         value={preferedEditor}
+        Options={["default", "mirrored", "legacy"]}
         onChange={(e) => {
           setPreferedEditor(e.target.value as editorPreference);
         }}
-        size="medium"
-      >
-        {editorPreferences.map((preference) => (
-          <MenuItem value={preference}>
-            <Typography textAlign="center">{preference}</Typography>
-          </MenuItem>
-        ))}
-      </Select>
-    </>
+      />
+      <PreferenceSelector
+        label="prefered editor"
+        value={preferedEditor}
+        Options={["default", "mirrored", "legacy"]}
+        onChange={(e) => {
+          setPreferedEditor(e.target.value as editorPreference);
+        }}
+      />
+    </Box>
   );
 };
 
