f7c73f838d5f1155a6e2986222b0f78e67127f34
itzsyboo <55495526+Fiona1121@users.noreply.github.com>
2022-01-29 16:24:31 +0800

    add types for effect list and update loadSlice for loading effect list variables

---
+++ editor/components/EffectList/index.tsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+
+export default function EffectList() {
+    return;
+}
--- editor/types/loadSlice.d.ts
+++ editor/types/loadSlice.d.ts
@@ -1,77 +1,82 @@
 import {
-  ControlMapType,
-  ControlRecordType,
-  posRecordType,
-  lightPresetsType,
-  posPresetsType,
+    ControlMapType,
+    ControlRecordType,
+    posRecordType,
+    lightPresetsType,
+    posPresetsType,
+    EffectRecordMapType,
+    EffectStatusMapType,
 } from "./globalSlice";
 interface BLPARTSTYPE {
-  [index: string]: {
-    prefix: string;
-    name: string;
-    postfix: string;
-  };
+    [index: string]: {
+        prefix: string;
+        name: string;
+        postfix: string;
+    };
 }
 interface ELPARTS extends BLPARTSTYPE {}
 interface LEDPARTS {
-  [index: string]: {
-    prefix: string;
-    name: string;
-    postfix: string[];
-  };
+    [index: string]: {
+        prefix: string;
+        name: string;
+        postfix: string[];
+    };
 }
 export interface textureType {
-  //refered to /data/texture.json
-  BLPARTS: BLPARTSTYPE;
-  ELPARTS: ELPARTSTYPE;
-  LEDPARTS: LEDPARTTYPE;
+    //refered to /data/texture.json
+    BLPARTS: BLPARTSTYPE;
+    ELPARTS: ELPARTSTYPE;
+    LEDPARTS: LEDPARTTYPE;
 }
 
 interface dancerPARTs {
-  [index: string]: {
-    zIndex: number;
-    width: number;
-    height: number;
-    x: number;
-    y: number;
-  };
+    [index: string]: {
+        zIndex: number;
+        width: number;
+        height: number;
+        x: number;
+        y: number;
+    };
 }
 interface dancerPARTs {
-  //refered to data/dancers
-  BLPARTS: dancerPARTs;
-  ELPARTS: dancerPARTs;
-  LEDPARTS: dancerPARTs;
+    //refered to data/dancers
+    BLPARTS: dancerPARTs;
+    ELPARTS: dancerPARTs;
+    LEDPARTS: dancerPARTs;
 }
 export interface DancersType {
-  [index: string]: dancerPARTs;
+    [index: string]: dancerPARTs;
 }
 
 export interface loadType {
-  // refered to /data/load.json""
-  Music: string;
-  Control: string;
-  ControlMap: string;
-  Position: string;
-  LightPresets: string;
-  PosPresets: string;
-  PosPresets: string;
-  Dancers: {
-    prefix: string;
-    postfix: string;
-    names: string[];
-  };
-  Texture: string;
+    // refered to /data/load.json""
+    Music: string;
+    Control: string;
+    ControlMap: string;
+    Position: string;
+    LightPresets: string;
+    PosPresets: string;
+    EffectRecordMap: string;
+    EffectStatueMap: string;
+    Dancers: {
+        prefix: string;
+        postfix: string;
+        names: string[];
+    };
+    Texture: string;
 }
 export interface loadState {
-  init: boolean;
-  music: string; // load music path
-  load: loadType;
-  control: ControlRecordType; // loaded control.json, may not be same as localStorage (this is for default)
-  controlMap: ControlMapType; // loaded controlMap.json
-  position: posRecordType; // loaded position.json, may not be same as localStorage (this is for default)
-  lightPresets: lightPresetsType; // loaded lightPresets.json, may not be same as localStorage (this is for default)
-  posPresets: posPresetsType; // loaded lightPresets.json, may not be same as localStorage (this is for default)
-  dancers: DancersType;
-  texture: textureType;
-  dancerNames: string[]; // [name]
+    init: boolean;
+    music: string; // load music path
+    load: loadType;
+    control: ControlRecordType; // loaded control.json, may not be same as localStorage (this is for default)
+    controlMap: ControlMapType; // loaded controlMap.json
+    position: posRecordType; // loaded position.json, may not be same as localStorage (this is for default)
+    lightPresets: lightPresetsType; // loaded lightPresets.json, may not be same as localStorage (this is for default)
+    posPresets: posPresetsType; // loaded posPresets.json, may not be same as localStorage (this is for default)
+    effectRecordMap: EffectRecordMapType; // loaded effectRecord.json
+    effectStatusMap: EffectStatusMapType; // loaded effectStatus.json
+    dancers: DancersType;
+    texture: textureType;
+    dancerNames: string[]; // [name]
 }
