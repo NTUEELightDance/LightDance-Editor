3f1f0fb85066ebfe6cf8b17128e34fd53e6751ae
JustinHuang1111 <b09901062@ntu.edu.tw>
2022-02-07 18:10:38 +0800

    EDITOR-#70 editor server trivial problems

--- editor-server/src/resolvers/types/colorMap.ts
+++ editor-server/src/resolvers/types/colorMap.ts
@@ -5,11 +5,16 @@ import db from "../../models";
 import redis from "../../redis";
 
 interface LooseObject {
+<<<<<<< HEAD
   [key: string]: any;
+=======
+    [key: string]: any;
+>>>>>>> 9aa804f (EDITOR-#70 editor server trivial problems)
 }
 
 @ObjectType()
 export class ColorMap {
+<<<<<<< HEAD
   @Field((type) => ColorMapScalar)
   colorMap: Object[];
 }
@@ -39,3 +44,34 @@ export const ColorMapScalar = new GraphQLScalarType({
     return ast.value; // value from the client query
   },
 });
+=======
+    @Field((type) => ColorMapScalar)
+    colorMap: Object[];
+}
+
+export const ColorMapScalar = new GraphQLScalarType({
+    name: "ColorMapCustomScalar",
+    description: "Color map scalar type",
+    async serialize(value: any) {
+        // check the type of received value
+        const result: LooseObject = {};
+        value.map((data: any) => {
+            const { color, colorCode } = data;
+            console.log(data);
+            result[color] = colorCode;
+        });
+        console.log(result);
+        return result;
+    },
+    parseValue(value: unknown): any {
+        // check the type of received value
+
+        return value; // value from the client input variables
+    },
+    parseLiteral(ast: any): any {
+        // check the type of received value
+
+        return ast.value; // value from the client query
+    },
+});
+>>>>>>> 9aa804f (EDITOR-#70 editor server trivial problems)
