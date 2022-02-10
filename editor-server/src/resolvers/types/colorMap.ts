a139054023565fe57b055e21b6e18f76ff7ce823
JustinHuang1111 <b09901062@ntu.edu.tw>
2022-02-10 21:51:49 +0800

    EDITOR-#70 format code

--- editor-server/src/resolvers/types/colorMap.ts
+++ editor-server/src/resolvers/types/colorMap.ts
@@ -5,37 +5,37 @@ import db from "../../models";
 import redis from "../../redis";
 
 interface LooseObject {
-    [key: string]: any;
+  [key: string]: any;
 }
 
 @ObjectType()
 export class ColorMap {
-    @Field((type) => ColorMapScalar)
-    colorMap: Object[];
+  @Field((type) => ColorMapScalar)
+  colorMap: Object[];
 }
 
 export const ColorMapScalar = new GraphQLScalarType({
-    name: "ColorMapCustomScalar",
-    description: "Color map scalar type",
-    async serialize(value: any) {
-        // check the type of received value
-        const result: LooseObject = {};
-        value.map((data: any) => {
-            const { color, colorCode } = data;
-            console.log(data);
-            result[color] = colorCode;
-        });
-        console.log(result);
-        return result;
-    },
-    parseValue(value: unknown): any {
-        // check the type of received value
+  name: "ColorMapCustomScalar",
+  description: "Color map scalar type",
+  async serialize(value: any) {
+    // check the type of received value
+    const result: LooseObject = {};
+    value.map((data: any) => {
+      const { color, colorCode } = data;
+      console.log(data);
+      result[color] = colorCode;
+    });
+    console.log(result);
+    return result;
+  },
+  parseValue(value: unknown): any {
+    // check the type of received value
 
-        return value; // value from the client input variables
-    },
-    parseLiteral(ast: any): any {
-        // check the type of received value
+    return value; // value from the client input variables
+  },
+  parseLiteral(ast: any): any {
+    // check the type of received value
 
-        return ast.value; // value from the client query
-    },
+    return ast.value; // value from the client query
+  },
 });
