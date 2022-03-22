1a2ebb7ea41ae83be2413fa2463fadc55c4edb6a
Mecoli1219 <72752478+Mecoli1219@users.noreply.github.com>
2022-03-23 01:43:59 +0800

    EDITOR-#270. fix effectlist bug

--- editor-server/src/models/EffectList.ts
+++ editor-server/src/models/EffectList.ts
@@ -3,13 +3,16 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 // Creating a schema, sort of like working with an ORM
-const EffectListSchema = new Schema({
-  start: { type: Number, required: [true, "start field is required."] },
-  end: { type: Number, required: [true, "start field is required."] },
-  description: { type: String, required: false },
-  controlFrames: { type: Object },
-  positionFrames: { type: Object },
-});
+const EffectListSchema = new Schema(
+  {
+    start: { type: Number, required: [true, "start field is required."] },
+    end: { type: Number, required: [true, "start field is required."] },
+    description: { type: String, required: false },
+    controlFrames: { type: Object, default: {} },
+    positionFrames: { type: Object, default: {} },
+  },
+  { minimize: false }
+);
 
 // Creating a table within database with the defined schema
 const EffectList = mongoose.model("EffectList", EffectListSchema);
