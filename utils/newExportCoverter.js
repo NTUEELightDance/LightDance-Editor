9a728c8b7ff9a086f181c704cd728547abe564c3
莊加旭 <76544194+zhuang-jia-xu@users.noreply.github.com>
2022-03-24 19:42:40 +0800

    EDITOR-#276 update models and export.json
    * update model and export.json
    
    * upload model, partName and remove bad color
    
    * remove console log
    
    * update load.json
    
    Co-authored-by: Mason Chen <b08901058@ntu.edu.tw>

--- utils/newExportCoverter.js
+++ utils/newExportCoverter.js
@@ -5,6 +5,7 @@
 const fs = require("fs");
 const { stat } = require("fs/promises");
 const { exit } = require("process");
+const _ = require("lodash");
 
 // Read Argument
 const args = process.argv; // 0: node, 1: posCenter.js
@@ -12,110 +13,164 @@ if (args.length < 4) {
   console.error(`[Error] Invalid Arguments !!!`);
   exit();
 }
-const json = args[2];
+const inputPath = args[2];
 const outputPath = args[3];
 
-console.log("Reading json from ... ", json);
+// get input file
+console.log("Reading json from ... ", inputPath);
 let raw = null;
-
 try {
-  raw = fs.readFileSync(json);
+  raw = fs.readFileSync(inputPath);
 } catch (err) {
-  console.error(`[Error] Can't open file ${json}`);
+  console.error(`[Error] Can't open file ${inputPath}`);
   exit();
 }
 
-const { position, control, dancer } = JSON.parse(raw);
+const {
+  position: originalPosition,
+  control: originalControl,
+  dancer,
+  color: COLOR,
+} = JSON.parse(raw);
 
 const YELLOW_DANCER = [
-  { name: "Calf_R", type: "FIBER" },
-  { name: "Thigh_R", type: "FIBER" },
-  { name: "LymphaticDuct_R", type: "FIBER" },
-  { name: "Waist_R", type: "FIBER" },
-  { name: "Arm_R", type: "FIBER" },
-  { name: "Shoulder_R", type: "FIBER" },
-  { name: "CollarBone_R", type: "FIBER" },
-  { name: "Chest", type: "FIBER" },
   { name: "Visor", type: "FIBER" },
-  { name: "Ear_R", type: "FIBER" },
-  { name: "Calf_L", type: "FIBER" },
-  { name: "Thigh_L", type: "FIBER" },
-  { name: "LymphaticDuct_L", type: "FIBER" },
-  { name: "Waist_L", type: "FIBER" },
-  { name: "CollarBone_L", type: "FIBER" },
-  { name: "Arm_L", type: "FIBER" },
-  { name: "Ear_L", type: "FIBER" },
+  { name: "Chest", type: "FIBER" },
+  { name: "Shoulder_R", type: "FIBER" },
   { name: "Shoulder_L", type: "FIBER" },
-  { name: "Glove_L", type: "FIBER" },
-  { name: "Glove_R", type: "FIBER" },
-  { name: "Glove_L_LED", type: "LED" },
-  { name: "Glove_R_LED", type: "LED" },
+  { name: "Arm_R", type: "FIBER" },
+  { name: "Arm_L", type: "FIBER" },
+  { name: "Waist_R", type: "FIBER" },
+  { name: "Waist_L", type: "FIBER" },
+  { name: "Thigh_R", type: "FIBER" },
+  { name: "Thigh_L", type: "FIBER" },
+  { name: "Calf_R", type: "FIBER" },
+  { name: "Calf_L", type: "FIBER" },
+
   { name: "Visor_LED", type: "LED" },
-  { name: "CollarBone_R_LED", type: "LED" },
-  { name: "CollarBone_L_LED", type: "LED" },
+  { name: "Shoulder_L_LED", type: "LED" },
+  { name: "Shoulder_R_LED", type: "LED" },
+  { name: "Hand_L_LED", type: "LED" },
+  { name: "Hand_R_LED", type: "LED" },
   { name: "Shoe_R_LED", type: "LED" },
   { name: "Shoe_L_LED", type: "LED" },
-  { name: "Shoulder_R_LED", type: "LED" },
-  { name: "Shoulder_L_LED", type: "LED" },
-  { name: "Hand_R_LED", type: "LED" },
-  { name: "Hand_L_LED", type: "LED" },
+
+  // merged
+  { name: "Ear", type: "FIBER" },
+  { name: "CollarBone", type: "FIBER" },
+  { name: "CollarBone_LED", type: "LED" },
+
+  // separated
+  { name: "LymphaticDuct_R_U", type: "FIBER" },
+  { name: "LymphaticDuct_R_D", type: "FIBER" },
+  { name: "LymphaticDuct_L_U", type: "FIBER" },
+  { name: "LymphaticDuct_L_D", type: "FIBER" },
+
+  // deleted
+  // fiber: glove
+  // LED: glove
 ];
 
 const RED_DANCER = [
-  { name: "Capacitor_1", type: "FIBER" },
-  { name: "Capacitor_2", type: "FIBER" },
-  { name: "Surcoat", type: "FIBER" },
-  { name: "Surcoat_L", type: "FIBER" },
-  { name: "Surcoat_R", type: "FIBER" },
-  { name: "Wrist_R", type: "FIBER" },
-  { name: "Tie", type: "FIBER" },
-  { name: "Boot_R", type: "FIBER" },
-  { name: "Mask", type: "FIBER" },
-  { name: "Eyes", type: "FIBER" },
-  { name: "Chest", type: "FIBER" },
-  { name: "Collar", type: "FIBER" },
+  // Arm ->
+  { name: "Shoulder_R", type: "FIBER" },
+  { name: "Shoulder_L", type: "FIBER" },
+
+  // Wrist ->
   { name: "Arm_R", type: "FIBER" },
   { name: "Arm_L", type: "FIBER" },
-  { name: "Wrist_L", type: "FIBER" },
-  { name: "Glove_L", type: "FIBER" },
-  { name: "Glove_R", type: "FIBER" },
-  { name: "Boot_L", type: "FIBER" },
-  { name: "Rune_LED", type: "LED" },
-  { name: "Rune_L_LED", type: "LED" },
-  { name: "Rune_R_LED", type: "LED" },
-  { name: "Shoe_R_LED", type: "LED" },
-  { name: "Shoe_L_LED", type: "LED" },
-  { name: "Chest_LED", type: "LED" },
-  { name: "Wrist_R_LED", type: "LED" },
-  { name: "Wrist_L_LED", type: "LED" },
+
+  // Surcoat L R
+  { name: "Cape_L", type: "FIBER" },
+  { name: "Cape_R", type: "FIBER" },
+
+  { name: "Tie", type: "FIBER" },
+  { name: "Rune", type: "FIBER" },
+
+  { name: "Shoe_L_U", type: "FIBER" },
+  { name: "Shoe_L_M", type: "FIBER" },
+  { name: "Shoe_L_D", type: "FIBER" },
+  { name: "Shoe_R_U", type: "FIBER" },
+  { name: "Shoe_R_M", type: "FIBER" },
+  { name: "Shoe_R_D", type: "FIBER" },
+
   { name: "Eyes_LED", type: "LED" },
+
+  { name: "CollarBone_LED", type: "LED" },
+
+  { name: "Wrist_L_LED", type: "LED" },
+  { name: "Wrist_R_LED", type: "LED" },
+
+  { name: "Shoe_L_LED", type: "LED" },
+  { name: "Shoe_R_LED", type: "LED" },
+  { name: "Cape_LED", type: "LED" },
+
+  // { name: "Capacitor_1", type: "FIBER" }, ->
+  { name: "Stomach_L", type: "FIBER" },
+  // { name: "Capacitor_2", type: "FIBER" }, ->
+  { name: "Stomach_R", type: "FIBER" },
+
+  // separated parts
+  { name: "Mask_1", type: "FIBER" },
+  { name: "Mask_2", type: "FIBER" },
+  // Chest ->
+  { name: "Chest", type: "FIBER" },
+  { name: "CollarBone_1", type: "FIBER" },
+  { name: "CollarBone_2", type: "FIBER" },
+
+  // deleted
+  // Collar
+  // { name: "Surcoat", type: "FIBER" },
 ];
 
-const BLUE_DANCER = [
-  { name: "Calf_R", type: "FIBER" },
-  { name: "Thigh_R", type: "FIBER" },
-  { name: "Arm_R", type: "FIBER" },
-  { name: "Thigh_L", type: "FIBER" },
-  { name: "Shoulder_R", type: "FIBER" },
-  { name: "Cuirass", type: "FIBER" },
-  { name: "Shoulder_L", type: "FIBER" },
-  { name: "Plackart", type: "FIBER" },
-  { name: "Belt", type: "FIBER" },
-  { name: "Helmet", type: "FIBER" },
-  { name: "Calf_L", type: "FIBER" },
-  { name: "Arm_L", type: "FIBER" },
-  { name: "Glove_L", type: "FIBER" },
-  { name: "Glove_R", type: "FIBER" },
-  { name: "Shoe_L", type: "FIBER" },
-  { name: "Shoe_R", type: "FIBER" },
-  { name: "Glove_L_LED", type: "LED" },
-  { name: "Glove_R_LED", type: "LED" },
-  { name: "Shoe_R_LED", type: "LED" },
-  { name: "Hand_R_LED", type: "LED" },
-  { name: "Hand_L_LED", type: "LED" },
-  { name: "Shoe_L_LED", type: "LED" },
+const CYAN_DANCER = [
   { name: "Shoulder_L_LED", type: "LED" },
   { name: "Shoulder_R_LED", type: "LED" },
+  { name: "Shoe_R_LED", type: "LED" },
+  { name: "Shoe_L_LED", type: "LED" },
+  { name: "Hand_R_LED", type: "LED" },
+  { name: "Hand_L_LED", type: "LED" },
+
+  // deleted
+  // { name: "Glove_L_LED", type: "LED" },
+  // { name: "Glove_R_LED", type: "LED" },
+
+  // separated
+  { name: "Helmet_1", type: "FIBER" },
+  { name: "Helmet_2", type: "FIBER" },
+  { name: "Helmet_3", type: "FIBER" },
+
+  { name: "Shoulder_L_1", type: "FIBER" },
+  { name: "Shoulder_L_2", type: "FIBER" },
+  { name: "Shoulder_R_1", type: "FIBER" },
+  { name: "Shoulder_R_2", type: "FIBER" },
+
+  { name: "Plackart_1", type: "FIBER" },
+  { name: "Plackart_2", type: "FIBER" },
+
+  { name: "Cuirass_1", type: "FIBER" },
+  { name: "Cuirass_2", type: "FIBER" },
+
+  { name: "Arm_L_1", type: "FIBER" },
+  { name: "Arm_L_2", type: "FIBER" },
+  { name: "Arm_R_1", type: "FIBER" },
+  { name: "Arm_R_2", type: "FIBER" },
+
+  { name: "Belt_1", type: "FIBER" },
+  { name: "Belt_2", type: "FIBER" },
+
+  { name: "Thigh_L_1", type: "FIBER" },
+  { name: "Thigh_L_2", type: "FIBER" },
+  { name: "Thigh_R_1", type: "FIBER" },
+  { name: "Thigh_R_2", type: "FIBER" },
+
+  { name: "Calf_L_1", type: "FIBER" },
+  { name: "Calf_L_2", type: "FIBER" },
+  { name: "Calf_R_1", type: "FIBER" },
+  { name: "Calf_R_2", type: "FIBER" },
+
+  // new parts
+  { name: "Eyes_LED", type: "LED" },
 ];
 
 const DANCER = [
@@ -124,147 +179,135 @@ const DANCER = [
   { parts: YELLOW_DANCER, name: "3_yeee" },
   { parts: YELLOW_DANCER, name: "4_breaKing" },
   { parts: YELLOW_DANCER, name: "5_itzyboo" },
-  { parts: BLUE_DANCER, name: "6_yu" },
-  { parts: BLUE_DANCER, name: "7_wang" },
-  { parts: BLUE_DANCER, name: "8_hao" },
-  { parts: BLUE_DANCER, name: "9_monkey" },
-  { parts: BLUE_DANCER, name: "10_dontstop" },
+  { parts: CYAN_DANCER, name: "6_yu" },
+  { parts: CYAN_DANCER, name: "7_wang" },
+  { parts: CYAN_DANCER, name: "8_hao" },
+  { parts: CYAN_DANCER, name: "9_monkey" },
+  { parts: CYAN_DANCER, name: "10_dontstop" },
   { parts: RED_DANCER, name: "11_wish" },
 ];
 
-const status = {};
-const old_pos = [
-  {
-    x: -306.96464010479804,
-    y: -184.2666717529297,
-    z: -184.2666717529297,
-  },
-  {
-    x: -160.96464010479804,
-    y: -180.93332824707034,
-    z: -180.93332824707034,
-  },
-  {
-    x: -36.964640104798036,
-    y: -81.59998474121095,
-    z: -81.59998474121095,
-  },
-  {
-    x: 0.3687034010613388,
-    y: -184.2666717529297,
-    z: -184.2666717529297,
-  },
-  {
-    x: -376.96464010479804,
-    y: -76.93332824707032,
-    z: -76.93332824707032,
-  },
-  {
-    x: -195.63129659893866,
-    y: -76.2666717529297,
-    z: -76.2666717529297,
-  },
-  {
-    x: 131.03535989520196,
-    y: -73.59998474121095,
-    z: -73.59998474121095,
-  },
-  {
-    x: 109.70204690692071,
-    y: -270.93332824707034,
-    z: -270.93332824707034,
-  },
-  { x: 123.03535989520196, y: -275.6, z: -275.6 },
-  {
-    x: 132.36873391863946,
-    y: -258.2666717529297,
-    z: -258.2666717529297,
-  },
-  {
-    x: 132.36873391863946,
-    y: -128.2666717529297,
-    z: -128.2666717529297,
-  },
-];
-
-const pos = {};
-const center = { x: 0, y: 0, z: 0 };
-
-DANCER.forEach((dancer, i) => {
-  const { name } = dancer;
-  pos[name] = {
-    x: old_pos[i].x / 30,
-    y: 0,
-    z: old_pos[i].z / 30,
-  };
-  center.x += old_pos[i].x / 30;
-  center.y += 0;
-  center.z += old_pos[i].z / 30;
+const RED_STATUS = {};
+RED_DANCER.forEach(({ name, type }) => {
+  RED_STATUS[name] =
+    type === "FIBER" ? { color: "red", alpha: 0 } : { src: "", alpha: 0 };
 });
 
-center.x /= 11;
-center.y /= 11;
-center.z /= 11;
+const partmaps = {
+  yellow: {
+    // merge
+    Ear_R: "Ear",
+    Ear_L: null,
+    CollarBone_R: "CollarBone",
+    CollarBone_L: null,
+    CollarBone_R_LED: "CollarBone_LED",
+    CollarBone_L_LED: null,
+    // separated
+    LymphaticDuct_R: ["LymphaticDuct_R_U", "LymphaticDuct_R_D"],
+    LymphaticDuct_L: ["LymphaticDuct_L_U", "LymphaticDuct_L_D"],
+    // deleted
+    Glove_R: null,
+    Glove_L: null,
+    Glove_R_LED: null,
+    Glove_L_LED: null,
+  },
+  cyan: {
+    // separated
+    Helmet: ["Helmet_1", "Helmet_2", "Helmet_3"],
 
-DANCER.forEach((dancer, i) => {
-  const { parts, name } = dancer;
-  status[name] = {};
-  parts.forEach((part) => {
-    switch (part.type) {
-      case "LED":
-        status[name][part.name] = { src: "", alpha: 0 };
-        break;
-      case "FIBER":
-        // status[name][part.name] = { color: "black", alpha: 0 };
-        const index = Number(name.split("_")[0]);
-        if (index <= 5 && index >= 0)
-          status[name][part.name] = { color: "yellow", alpha: 1 };
-        if (index <= 10 && index >= 6)
-          status[name][part.name] = { color: "blue", alpha: 1 };
-        if (index === 11) status[name][part.name] = { color: "red", alpha: 1 };
-        break;
-      default:
+    Shoulder_L: ["Shoulder_L_1", "Shoulder_L_2"],
+    Shoulder_R: ["Shoulder_R_1", "Shoulder_R_2"],
+
+    Plackart: ["Plackart_1", "Plackart_2"],
+
+    Cuirass: ["Cuirass_1", "Cuirass_2"],
+
+    Arm_L: ["Arm_L_1", "Arm_L_2"],
+    Arm_R: ["Arm_R_1", "Arm_R_2"],
+
+    Belt: ["Belt_1", "Belt_2"],
+
+    Thigh_L: ["Thigh_L_1", "Thigh_L_2"],
+    Thigh_R: ["Thigh_R_1", "Thigh_R_2"],
+
+    Calf_L: ["Calf_L_1", "Calf_L_2"],
+    Calf_R: ["Calf_R_1", "Calf_R_2"],
+
+    Shoe_R: null,
+    Shoe_L: null,
+
+    Glove_R: null,
+    Glove_L: null,
+
+    Glove_R_LED: null,
+    Glove_L_LED: null,
+
+    new: ["Eyes_LED"],
+  },
+  red: null,
+};
+
+const dancerType = {
+  "1_chi": "yellow",
+  "2_meow": "yellow",
+  "3_yeee": "yellow",
+  "4_breaKing": "yellow",
+  "5_itzyboo": "yellow",
+  "6_yu": "cyan",
+  "7_wang": "cyan",
+  "8_hao": "cyan",
+  "9_monkey": "cyan",
+  "10_dontstop": "cyan",
+  "11_wish": "red",
+};
+
+const CONTROL = {};
+Object.entries(originalControl).forEach(([frameKey, value]) => {
+  const newStatus = {};
+  Object.entries(value.status).forEach(([dancerName, dancerStatus]) => {
+    const map = partmaps[dancerType[dancerName]];
+
+    if (!map) {
+      newStatus[dancerName] = _.cloneDeep(RED_STATUS);
+      return;
     }
+
+    newStatus[dancerName] = {};
+
+    if ("new" in map) {
+      map.new.forEach((newPart) => {
+        const isLED = newPart.endsWith("_LED");
+        newStatus[dancerName][newPart] = isLED
+          ? { src: "", alpha: 0 }
+          : { color: dancerType[dancerName], alpha: 0 };
+      });
+    }
+
+    Object.entries(dancerStatus).forEach(([partName, partStatus]) => {
+      if (partName in map) {
+        const newName = map[partName];
+        // if newName is null, delete the part
+        if (!newName) return;
+        else if (typeof newName === "string")
+          newStatus[dancerName][newName] = { ...partStatus };
+        else if (Array.isArray(newName))
+          newName.forEach(
+            (name) => (newStatus[dancerName][name] = { ...partStatus })
+          );
+      } else {
+        newStatus[dancerName][partName] = { ...partStatus };
+      }
+    });
   });
-  pos[name] = {
-    x: old_pos[i].x / 30 - center.x,
-    y: 0,
-    z: old_pos[i].z / 30 - center.z,
-  };
+  CONTROL[frameKey] = { ...value, status: newStatus };
 });
-
-console.log(center);
-
-const CONTROL = {
-  "01HvN": {
-    fade: false,
-    start: 0,
-    status,
-  },
-};
-
-const POSITION = {
-  "0006T": {
-    start: 0,
-    pos,
-  },
-};
-
-const COLOR = {
-  black: "#000000",
-  red: "#FF04FF",
-  blue: "#05FFFF",
-  yellow: "#FFFF2d",
-};
 
 const exportJSON = {
-  position: POSITION,
+  position: originalPosition,
   control: CONTROL,
   dancer: DANCER,
   color: COLOR,
 };
-
-console.log(exportJSON);
 
 fs.writeFile(outputPath, JSON.stringify(exportJSON), () => {
   console.log(`Writing new file to ... ${outputPath}`);
