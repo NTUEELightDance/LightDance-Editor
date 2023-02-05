6aa36fcf368451980b4404aa1f880134d08db313
dienruei123 <dienruei@gmail.com>
2023-02-05 15:48:08 +0800

    EDITOR-#356. Add new initDB, saveData, eslint commands in utils, fix minor issue

---
+++ utils/saveData.js
@@ -0,0 +1,27 @@
+/**
+ * node saveData.js
+ */
+const axios = require("axios")
+const fs = require("fs")
+
+const filePath = process.argv[2]
+
+const instance = axios.create({
+  baseURL: "http://localhost:4000/",
+  maxContentLength: Infinity,
+  maxBodyLength: Infinity,
+})
+
+async function main() {
+  const file = fs.createWriteStream(filePath)
+
+  const response = await instance.get("/api/exportData")
+  //   console.log(response.data)
+  if (response.status === 200) {
+    file.write(JSON.stringify(response.data))
+    console.log(`Data saved to '${filePath}' successfully!!`)
+  }
+  file.close()
+}
+
+main().catch((error) => console.error(error))
