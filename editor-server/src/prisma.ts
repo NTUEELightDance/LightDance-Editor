8580986c296404f360a89559b38919bb68b97b90
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-02-11 02:08:54 +0800

    add create user script

--- editor-server/src/prisma.ts
+++ editor-server/src/prisma.ts
@@ -9,14 +9,55 @@ export const createUser = async (
   username: string,
   password: string
 ): Promise<void> => {
-  // if the user already exists, throw an error
+  // throw an error if the username or password is empty
+  if (username.length === 0 || password.length === 0) {
+    throw new Error("Username and password are required.");
+  }
+
+  // throw an error if the username contains a space
+  if (username.includes(" ")) {
+    throw new Error("Username cannot contain spaces.");
+  }
+
+  // throw an error if the username is too long
+  if (username.length > 30) {
+    throw new Error("Username cannot be longer than 30 characters.");
+  }
+
+  // throw an error if the password is too long
+  if (password.length > 100) {
+    throw new Error("Password cannot be longer than 100 characters.");
+  }
+
+  // throw an error if try to create a user with the same name as the admin and it already exists
+  if (username === process.env.ADMIN_USERNAME) {
+    const admin = await prisma.user.findUnique({
+      where: {
+        name: username,
+      },
+    });
+    if (admin) {
+      throw new Error("Admin user already exists.");
+    }
+  }
+
+  // if the user already exists, update the password
   const user = await prisma.user.findUnique({
     where: {
       name: username,
     },
   });
   if (user) {
-    throw new Error(`User "${username}" already exists.`);
+    const hash = await bcrypt.hash(password, HASH_ROUNDS);
+    await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        password: hash,
+      },
+    });
+    return;
   }
 
   const hash = await bcrypt.hash(password, HASH_ROUNDS);
