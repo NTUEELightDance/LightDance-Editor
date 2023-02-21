b62a5e9c0ebc230077b7bca7b424c21f2786db9f
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-02-21 17:49:56 +0800

    update register action's type to ensure type safety for actions

--- editor/core/registerActions/index.ts
+++ editor/core/registerActions/index.ts
@@ -9,28 +9,39 @@ import { debug } from "@/core/utils";
 /**
  * A mapping of actionName to the wrapped action.
  */
-type WrappedActionRegistry = Record<
-  string,
-  (payloadOptions?: PayloadOptions) => Promise<void>
->;
+type WrappedActionRegistry<T extends Record<string, Action>> = {
+  // if it is not undefined
+  [K in keyof T]: Parameters<T[K]>[1] extends number | string | boolean | object
+    ?
+        | ((payloadOption: PayloadOptions<T[K]>) => void)
+        | ((payloadOption: PayloadOptions<T[K]>) => Promise<void>)
+    :
+        | ((payloadOption?: PayloadOptions<T[K]>) => void)
+        | ((payloadOption?: PayloadOptions<T[K]>) => Promise<void>);
+};
 
-/**
- * PayloadOptions
- */
-interface PayloadOptions {
-  payload?: Payload;
-  options?: Options;
-}
+type PayloadOptions<A extends Action> = Parameters<A>[1] extends
+  | number
+  | string
+  | boolean
+  | object
+  ? {
+      payload: Parameters<A>[1];
+      options?: Options;
+    }
+  : {
+      payload?: never;
+      options?: Options;
+    };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Payload = any;
-
 /**
  * Options for determining whether to rerender or simulate after completing the action.
  */
 interface Options {
   rerender?: boolean; // to rerender React components
-  states?: string[]; // rerender according the states. If rerender but emtpy states, we will rerender the changed states automatically
+  states?: string[]; // rerender according the states. If rerender but empty states, we will rerender the changed states automatically
 
   refreshWavesurfer?: boolean;
   refreshThreeSimulator?: boolean;
@@ -41,7 +52,9 @@ interface Options {
  * An action will manipulate the states.
  * The action will be wrapped by actionCreator and saved in wrappedActionRegistry.
  */
-type Action = (state: State, payload: Payload) => void;
+type Action =
+  | ((state: State, payload: Payload) => void)
+  | ((state: State, payload: Payload) => Promise<void>);
 
 /**
  * Default options for the action
@@ -59,8 +72,8 @@ const defaultOptions = {
  * @param action functions with two parameters, state and payload
  * @returns a function with two parameters, payload and options
  */
-function actionCreator(action: Action, actionName: string) {
-  return async (payloadOptions?: PayloadOptions) => {
+function actionCreator<A extends Action>(action: Action, actionName: string) {
+  return async (payloadOptions?: PayloadOptions<A>) => {
     await action(state, payloadOptions?.payload);
     const options = { ...defaultOptions, ...payloadOptions?.options };
     debug(
@@ -100,16 +113,16 @@ function actionCreator(action: Action, actionName: string) {
  * @param actions
  * @returns
  */
-export function registerActions(actions: Record<string, Action>) {
+export function registerActions<T extends Record<string, Action>>(actions: T) {
   // Registry that stores all the actions.
-  const wrappedActionRegistry: WrappedActionRegistry = {};
-  Object.keys(actions).forEach((actionName: string) => {
+  const wrappedActionRegistry = {} as WrappedActionRegistry<T>;
+  Object.keys(actions).forEach((actionName: keyof T) => {
     if (actionName in wrappedActionRegistry) {
       throw new Error("The action has been registered !");
     }
     wrappedActionRegistry[actionName] = actionCreator(
       actions[actionName],
-      actionName
+      actionName as string
     );
   });
   debug(wrappedActionRegistry);
