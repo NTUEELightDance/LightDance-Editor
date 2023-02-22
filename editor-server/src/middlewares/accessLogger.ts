6644b84ba39b7fdaa69384a5ce76058b8d4047c8
Chun-Mao, Lai <72752478+Mecoli1219@users.noreply.github.com>
2023-02-22 19:05:52 +0800

    EDITOR-#397 Backend Add logger back (#398)

--- editor-server/src/middlewares/accessLogger.ts
+++ editor-server/src/middlewares/accessLogger.ts
@@ -1,36 +1,41 @@
 import { MiddlewareFn } from "type-graphql";
 import { TContext } from "../types/global";
+import prisma from "../prisma";
+import { Prisma } from "@prisma/client";
 
 // TODO: Implement this
 export const AccessMiddleware: MiddlewareFn<TContext> = async (
   { info, context },
   next
 ) => {
-  // const { fieldName, parentType, variableValues } = info;
-  // try {
-  //   if (info.parentType.name === "Mutation") {
-  //     const result = await next();
-  //     await new context.db.Logger({
-  //       user: context.userID,
-  //       variableValues,
-  //       type: parentType,
-  //       fieldName,
-  //       status: "Success",
-  //       result,
-  //     }).save();
-  //   } else {
-  //     await next();
-  //   }
-  // } catch (errorMessage) {
-  //   await new context.db.Logger({
-  //     user: context.userID,
-  //     variableValues,
-  //     type: parentType,
-  //     fieldName,
-  //     status: "Error",
-  //     errorMessage,
-  //   }).save();
-  //   throw errorMessage;
-  // }
-  await next();
+  const { fieldName, parentType, variableValues } = info;
+  try {
+    if (info.parentType.name === "Mutation") {
+      const result = await next();
+      await prisma.logger.create({
+        data: {
+          user: context.userId,
+          variableValue: variableValues,
+          // type: parentType,
+          fieldName,
+          status: "Success",
+          result,
+        },
+      });
+    } else {
+      await next();
+    }
+  } catch (errorMessage) {
+    await prisma.logger.create({
+      data: {
+        user: context.userId,
+        variableValue: variableValues,
+        // type: parentType,
+        fieldName,
+        status: "Error",
+        errorMessage: { message: String(errorMessage) },
+      },
+    });
+  }
+  // await next();
 };
