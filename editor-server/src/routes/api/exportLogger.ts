83491b1c965f52087882ecf2c181d870fcafb284
Mecoli1219 <michaellai901026@gmail.com>
2023-02-22 19:03:37 +0800

    EDITOR-#397 Backend Add logger back

--- editor-server/src/routes/api/exportLogger.ts
+++ editor-server/src/routes/api/exportLogger.ts
@@ -1,36 +1,36 @@
 import { Request, Response } from "express";
-// import { stringify } from "csv-stringify/sync";
-
-// import db from "../../models";
-// import { ILogger } from "../../types/global";
+import { Logger } from "../../../prisma/generated/type-graphql";
+import prisma from "../../prisma";
+import { stringify } from "csv-stringify/sync";
 
 // TODO: Implement this
 const exportLogger = async (req: Request, res: Response) => {
-  // try {
-  //   const loggers = await db.Logger.find().sort({ time: -1 });
-  //   const rows = [
-  //     ["Time", "FieldName", "User", "Status", "ID", "Variable Values"],
-  //   ];
-  //   loggers.forEach((logger: ILogger) => {
-  //     const { time, fieldName, user, status, variableValues } = logger;
-  //     const _id = logger._id!;
-  //     rows.push([
-  //       time.toLocaleString(),
-  //       fieldName,
-  //       user,
-  //       status,
-  //       _id.toString(),
-  //       JSON.stringify(variableValues),
-  //     ]);
-  //   });
-  //   const output = stringify(rows);
-  //   res.setHeader("content-type", "application/csv");
-  //   res.setHeader("content-disposition", "attachment; filename=logger.csv");
-  //   res.send(output);
-  // } catch (err) {
-  //   res.status(404).send({ err });
-  // }
-  res.status(404).send({ err: "Not implemented" });
+  try {
+    const loggers = await prisma.logger.findMany({
+      orderBy: { time: "desc" },
+    });
+    const rows = [
+      ["Time", "FieldName", "User", "Status", "ID", "Variable Values"],
+    ];
+    loggers.forEach((logger: Logger) => {
+      const { id, time, fieldName, user, status, variableValue } = logger;
+      rows.push([
+        time.toLocaleString(),
+        fieldName,
+        user.toString(),
+        status,
+        id.toString(),
+        JSON.stringify(variableValue),
+      ]);
+    });
+    const output = stringify(rows);
+    res.setHeader("content-type", "application/csv");
+    res.setHeader("content-disposition", "attachment; filename=logger.csv");
+    res.send(output);
+  } catch (err) {
+    res.status(404).send({ err });
+  }
+  // res.status(404).send({ err: "Not implemented" });
 };
 
 export default exportLogger;
