b7c840329cc2bf3d445fcac723d09a8ea9e7fca0
walkerhsu <walkerhsu0808@gmail.com>
2023-02-23 15:09:55 +0800

    Editor-#369 HotKey cheat sheet done

--- editor/components/Settings/Hotkeys/index.tsx
+++ editor/components/Settings/Hotkeys/index.tsx
@@ -1,15 +1,25 @@
-import {
-  TableContainer,
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  Paper,
-  Box,
-} from "@mui/material";
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { TableVirtuoso, TableComponents } from "react-virtuoso";
 
-const HotKeyList = [
+interface HotKey {
+  label: string;
+  key: string;
+}
+
+interface ColumnData {
+  label: string;
+  width: string;
+  dataKey: keyof HotKey;
+}
+
+const HotKeyList: HotKey[] = [
   {
     label: "copy the selected dancer's lightProps",
     key: "ctrl/cmd+c",
@@ -34,40 +44,114 @@ const HotKeyList = [
     label: "select all dancers",
     key: "ctrl/cmd+a",
   },
+  {
+    label: "play/pause",
+    key: "space",
+  },
+  {
+    label: "next frame",
+    key: "right arrow / w",
+  },
+  {
+    label: "previous frame",
+    key: "left arrow / q",
+  },
+  {
+    label: "start editing",
+    key: "e",
+  },
+  {
+    label: "add a frame",
+    key: "a",
+  },
+  {
+    label: "delete a frame",
+    key: "del",
+  },
+  {
+    label: "",
+    key: "esc",
+  },
+  {
+    label: "switch between position editor and control editor",
+    key: "v",
+  },
+  {
+    label: "",
+    key: "g",
+  },
 ];
+
+const columns: ColumnData[] = [
+  {
+    width: "30%",
+    label: "HOTKEYS",
+    dataKey: "key",
+  },
+  {
+    width: "70%",
+    label: "FUNCTIONS",
+    dataKey: "label",
+  },
+];
+
+const VirtuosoTableComponents: TableComponents<HotKey> = {
+  // eslint-disable-next-line react/display-name
+  Scroller: React.forwardRef<HTMLDivElement>((props, ref) => (
+    <TableContainer component={Paper} {...props} ref={ref} />
+  )),
+  Table: (props) => (
+    <Table
+      {...props}
+      sx={{ borderCollapse: "separate", tableLayout: "fixed" }}
+    />
+  ),
+  TableHead,
+  TableRow: ({ ...props }) => <TableRow {...props} />,
+  // eslint-disable-next-line react/display-name
+  TableBody: React.forwardRef<HTMLTableSectionElement>((props, ref) => (
+    <TableBody {...props} ref={ref} />
+  )),
+};
+
+function fixedHeaderContent() {
+  return (
+    <TableRow>
+      {columns.map((column) => (
+        <TableCell
+          key={column.dataKey}
+          variant="head"
+          style={{ width: column.width }}
+          sx={{
+            backgroundColor: "background.paper",
+          }}
+        >
+          {column.label}
+        </TableCell>
+      ))}
+    </TableRow>
+  );
+}
+
+function rowContent(_index: number, row: HotKey) {
+  return (
+    <React.Fragment>
+      {columns.map((column) => (
+        <TableCell key={column.dataKey}>{row[column.dataKey]}</TableCell>
+      ))}
+    </React.Fragment>
+  );
+}
+
 export default function HotKeys() {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        width: "100%",
-        alignItems: "center",
-        justifyContent: "space-between",
-      }}
-    >
-      <TableContainer component={Paper}>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>HOTKEYS</TableCell>
-              <TableCell align="right">FUNCTION</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {HotKeyList.map(({ label, key }) => (
-              <TableRow
-                key={key}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {key}
-                </TableCell>
-                <TableCell align="right">{label}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Box>
+    <Paper style={{ height: 380, width: "100%" }}>
+      <TableVirtuoso
+        data={HotKeyList}
+        components={VirtuosoTableComponents}
+        fixedHeaderContent={fixedHeaderContent}
+        itemContent={rowContent}
+      />
+    </Paper>
   );
 }
