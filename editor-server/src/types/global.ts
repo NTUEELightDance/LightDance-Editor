751af95c90714f06ca03c15ed22b3772a60bb93f
陳柏睿 <90591931+rakechen-0307@users.noreply.github.com>
2023-02-24 21:54:05 +0800

    EDITOR-#401 Backend: Enable rename of color & ledEffect (#403)

--- editor-server/src/types/global.ts
+++ editor-server/src/types/global.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 export interface ConnectionParam extends Record<string, unknown> {
   token: string;
