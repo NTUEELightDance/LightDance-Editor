7c55460ce73432ca21f2a20daf5d407627c9ded0
謝沅瓏 <hsiehyuanlung@xieyuanlongde-MacBook-Pro.local>
2023-02-24 20:03:50 +0800

    EDITOR-#402 Add name, moedl, parts, start time elements to dialog

--- editor/components/LEDEffectList/LEDEffectDialog.tsx
+++ editor/components/LEDEffectList/LEDEffectDialog.tsx
@@ -7,6 +7,10 @@ import {
   Button,
   Box,
   Typography,
+  ToggleButtonGroup,
+  ToggleButton,
+  Grid,
+  Paper,
 } from "@mui/material";
 
 export interface LEDEffectDialogProps {
@@ -18,11 +22,64 @@ export default function LEFDffectDialog({
   addDialogOpen,
   handleClose,
 }: LEDEffectDialogProps) {
+  let displayModels = ["a", "b", "c", "d", "e", "f"];
+  let displayLEDParts = ["1", "2", "3", "4", "5", "6", "7"];
   return (
     <div>
-      <Dialog open={addDialogOpen} onClose={handleClose}>
-        <p>{`${addDialogOpen}`}</p>
-      </Dialog>
+      <Paper>
+        <Dialog open={addDialogOpen} onClose={handleClose}>
+          <DialogTitle>New LED Effect</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="New Effect Name"
+              required
+              fullWidth
+              variant="standard"
+            />
+            <Typography>MODELS</Typography>
+            <ToggleButtonGroup exclusive={true}>
+              <Grid container={true} spacing={1.25}>
+                {displayModels.map((v) => (
+                  <Grid key={v} item={true}>
+                    <ToggleButton value={v} key={v}>
+                      {v}
+                    </ToggleButton>
+                  </Grid>
+                ))}
+              </Grid>
+            </ToggleButtonGroup>
+
+            <Typography>PARTS</Typography>
+            <ToggleButtonGroup exclusive={true}>
+              <Grid container={true} spacing={1.25}>
+                {displayLEDParts.map((v) => (
+                  <Grid key={v} item={true}>
+                    <ToggleButton value={v} key={v}>
+                      {v}
+                    </ToggleButton>
+                  </Grid>
+                ))}
+              </Grid>
+            </ToggleButtonGroup>
+
+            <TextField
+              margin="normal"
+              id="name"
+              label="From Time:"
+              sx={{ width: "20em", marginRight: 2 }}
+              variant="outlined"
+              required
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button>Cancel</Button>
+            <Button>Add</Button>
+          </DialogActions>
+        </Dialog>
+      </Paper>
     </div>
   );
 }
