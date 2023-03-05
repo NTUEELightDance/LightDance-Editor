10a9e53f427dcb4b7847cd6f63189988d6d92090
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-03-06 06:50:59 +0800

    fix some typescript problem

--- editor/types/loadSlice.ts
+++ editor/types/loadSlice.ts
@@ -1,15 +1,33 @@
+import type {
+  LightPresetsType,
+  PosPresetsType,
+} from "@/components/Presets/presets";
+
 export interface LoadType {
-  // refered to /data/load.json""
+  // refer to /data/load.json""
   Music: string;
   LightPresets: string;
   PosPresets: string;
-  DancerMap: AnyAction;
+  DancerMap: Record<
+    string,
+    {
+      url: string;
+      modelName: string;
+    }
+  >;
 }
+
 export interface LoadState {
   init: boolean;
   music: string; // load music path
   load: LoadType;
   lightPresets: LightPresetsType; // loaded lightPresets.json, may not be same as localStorage (this is for default)
   posPresets: PosPresetsType; // loaded lightPresets.json, may not be same as localStorage (this is for default)
-  dancerMap: Any;
+  dancerMap: Record<
+    string,
+    {
+      url: string;
+      modelName: string;
+    }
+  >;
 }
