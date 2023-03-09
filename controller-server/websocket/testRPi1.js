ba5aae67ec28ac78bd73d76e45f4440c9b88f95f
pinru <b10901171@g.ntu.edu.tw>
2023-03-09 22:55:24 +0800

    Editor-#426 Controller server: Modify testRPi.js to fit new communication

--- controller-server/websocket/testRPi1.js
+++ controller-server/websocket/testRPi1.js
@@ -5,13 +5,13 @@ const socket = new WebSocket("ws://localhost:8082");
 socket.addEventListener("open", evnt => {
     console.log("Client RPi 1 websocket open on port ws://localhost:8082");
     socket.send(JSON.stringify({
-        command: "boardInfo",
-        payload: {
-            type:  "RPi",
-            info: {
-                dancerName: "Justin",
-                ip: "127.0.0",
-                hostName: "light-dance",
+        "command": "boardInfo",
+        "status": 0,
+        "payload": 
+        {
+            "type":  "RPi",
+            "info": {
+                "macaddr" : "1C:4D:70:0A:60:40",
             }
         }
     }));
