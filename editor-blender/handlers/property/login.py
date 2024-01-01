2d50a3832544af3f82780e35c84c5d844b018e69
tom1484 <tomchen2003611@gmail.com>
2024-01-01 23:40:35 +0800

    add basic structure

---
+++ editor-blender/handlers/property/login.py
@@ -0,0 +1,9 @@
+import bpy
+
+
+from ...core.states import states
+
+
+def update_is_running(self: bpy.types.WindowManager, context: bpy.types.Context):
+    states.is_running = getattr(
+        context.window_manager, "ld_is_runnning", False)
