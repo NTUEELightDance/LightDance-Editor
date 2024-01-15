68d1ca8be20d4ff505bc99b77e7787c58cbea3c9
Chalkman071 <josephtseng2004@gmail.com>
2024-01-15 19:39:50 +0800

    changed variable names + precommit

--- editor-blender/operators/color_palette/__init__.py
+++ editor-blender/operators/color_palette/__init__.py
@@ -1,11 +1,13 @@
 import bpy
-from gql import gql, Client
+from gql import Client, gql
 from gql.transport.aiohttp import AIOHTTPTransport
+
+from ...api.color_agent import color_agent
 from ...properties.color_palette import ld_ColorItem
 from ..async_core import AsyncOperator
-from ...api.color_agent import color_agent
 
-def add_color(colorName: str, colorCode: list):
+
+def add_color(color_name: str, color_code: list):
     transport = AIOHTTPTransport(url="http://localhost:4000/graphql")
 
     client = Client(transport=transport, fetch_schema_from_transport=True)
@@ -21,20 +23,14 @@ def add_color(colorName: str, colorCode: list):
         }
         """
     )
-    
-    variable = {
-        "color": {
-            "color": colorName,
-            "colorCode": {
-            "set": colorCode
-            }
-        }
-    }
-    
+
+    variable = {"color": {"color": color_name, "colorCode": {"set": color_code}}}
+
     response = client.execute(mutation, variable)
     return response
 
-def delete_color(colorId: int):
+
+def delete_color(color_id: int):
     transport = AIOHTTPTransport(url="http://localhost:4000/graphql")
 
     client = Client(transport=transport, fetch_schema_from_transport=True)
@@ -50,15 +46,14 @@ def delete_color(colorId: int):
         }
         """
     )
-    
-    variable = {
-    "deleteColorId": colorId
-    }
-    
+
+    variable = {"deleteColorId": color_id}
+
     response = client.execute(mutation, variable)
     return response
 
-def edit_color(colorId: int, colorName: str, colorCode: list):
+
+def edit_color(color_id: int, color_name: str, color_code: list):
     transport = AIOHTTPTransport(url="http://localhost:4000/graphql")
     client = Client(transport=transport, fetch_schema_from_transport=True)
     mutation = gql(
@@ -73,20 +68,14 @@ def edit_color(colorId: int, colorName: str, colorCode: list):
         """
     )
     variable = {
-        "data": {
-            "colorCode": {
-                "set": colorCode
-            },
-            "color": {
-                "set": colorName
-            }
-        },
-        "editColorId": colorId
-    }   
+        "data": {"colorCode": {"set": color_code}, "color": {"set": color_name}},
+        "editColorId": color_id,
+    }
     response = client.execute(mutation, variable)
     return response
 
-def refreshColormap():
+
+def refresh_colormap():
     transport = AIOHTTPTransport(url="http://localhost:4000/graphql")
     client = Client(transport=transport, fetch_schema_from_transport=True)
     query = gql(
@@ -98,146 +87,172 @@ def refreshColormap():
         }
         """
     )
-    colormap = client.execute(query)['colorMap']['colorMap']
-    setattr(bpy.types.WindowManager,"ld_ColorPalette", bpy.props.CollectionProperty(type=ld_ColorItem))
-    getattr(bpy.context.window_manager,"ld_ColorPalette").clear()
+    colormap = client.execute(query)["colorMap"]["colorMap"]
+    setattr(
+        bpy.types.WindowManager,
+        "ld_ColorPalette",
+        bpy.props.CollectionProperty(type=ld_ColorItem),
+    )
+    getattr(bpy.context.window_manager, "ld_ColorPalette").clear()
     for key in colormap:
-        item = getattr(bpy.context.window_manager,"ld_ColorPalette").add()
-        item.colorId = key
-        item.colorName = colormap[key]['color']
-        item.colorCode = colormap[key]['colorCode']
-        item.colorFloat = [x/255 for x in item.colorCode]
-        item.colorAlpha = 1.0
-    
+        item = getattr(bpy.context.window_manager, "ld_ColorPalette").add()
+        item.color_id = key
+        item.color_name = colormap[key]["color"]
+        item.color_code = colormap[key]["colorCode"]
+        item.color_float = [x / 255 for x in item.color_code]
+        item.color_alpha = 1.0
+
+
 def handle_colorEdit(context, editing_index):
-    panel = getattr(bpy.types,"LD_PT_color_palette")
+    panel = getattr(bpy.types, "LD_PT_color_palette")
     panel.editing_index = editing_index
     print(f"editing {editing_index}")
 
-def handle_colorNew(context): # may not needed
-    print('adding color')
+
+def handle_colorNew(context):  # may not needed
+    print("adding color")
+
 
 async def handle_colorDelete(index: int):
     print(f"delete color {index}")
     res = await color_agent.delete_color(index)
-    return res['deleteColor'] # type: ignore
+    return res["deleteColor"]  # type: ignore
+
 
 def handle_colorCancel(context):
     print("edit cancelled")
 
+
 async def handle_colorConfirm(editing_state):
     # TODO: types
-    color_temp = getattr(bpy.context.window_manager,"ld_ColorPalette_temp")[0]
+    color_temp = getattr(bpy.context.window_manager, "ld_ColorPalette_temp")[0]
     print("confirmed change")
     if editing_state == "EDIT":
-        response = await color_agent.edit_color(int(color_temp['colorId']), str(color_temp['colorName']), tuple(color_temp['colorCode'])) # type: ignore
-        return response['editColor'] # type: ignore
+        response = await color_agent.edit_color(int(color_temp["color_id"]), str(color_temp["color_name"]), tuple(color_temp["color_code"]))  # type: ignore
+        return response["editColor"]  # type: ignore
     elif editing_state == "NEW":
-        response = await color_agent.add_color(str(color_temp['colorName']), tuple(color_temp['colorCode'])) # type: ignore
-        return response['addColor'] # type: ignore
+        response = await color_agent.add_color(str(color_temp["color_name"]), tuple(color_temp["color_code"]))  # type: ignore
+        return response["addColor"]  # type: ignore
 
-## Define operators
 
-class ld_colorEditOperator(bpy.types.Operator):
+class ColorEditOperator(bpy.types.Operator):
     bl_idname = "object.ld_color_edit"
     bl_label = "Edit"
-    editing_index: bpy.props.IntProperty() # type: ignore
-    
+    editing_index: bpy.props.IntProperty()  # type: ignore
+
     @classmethod
     def poll(cls, context):
         return context.active_object is not None
-    
+
     def execute(self, context):
         handle_colorEdit(context, self.editing_index)
         panel = getattr(bpy.types, "LD_PT_color_palette")
         panel.editing_mode = True
         panel.editing_index = self.editing_index
         panel.editing_state = "EDIT"
-        color_temp = getattr(bpy.context.window_manager,"ld_ColorPalette_temp")[0]
-        color_edit = getattr(context.window_manager,"ld_ColorPalette")[self.editing_index]
-        color_temp['colorCode'] = color_edit['colorCode']
-        color_temp['colorFloat'] = color_edit['colorFloat']
-        color_temp['colorName'] = color_edit['colorName']
-        color_temp['colorId'] = color_edit['colorId']
-        return {'FINISHED'}
+        color_temp = getattr(bpy.context.window_manager, "ld_ColorPalette_temp")[0]
+        color_edit = getattr(context.window_manager, "ld_ColorPalette")[
+            self.editing_index
+        ]
+        color_temp["color_code"] = color_edit["color_code"]
+        color_temp["color_float"] = color_edit["color_float"]
+        color_temp["color_name"] = color_edit["color_name"]
+        color_temp["color_id"] = color_edit["color_id"]
+        return {"FINISHED"}
 
-class ld_colorNewOperator(bpy.types.Operator):
+
+class ColorNewOperator(bpy.types.Operator):
     bl_idname = "object.ld_color_new"
     bl_label = "New"
-    
+
     @classmethod
     def poll(cls, context):
         return context.active_object is not None
-    
+
     def execute(self, context):
         handle_colorNew(context)
-        panel = getattr(bpy.types,"LD_PT_color_palette")
+        panel = getattr(bpy.types, "LD_PT_color_palette")
         panel.editing_mode = True
         panel.editing_state = "NEW"
-        color_temp = getattr(bpy.context.window_manager,"ld_ColorPalette_temp")[0]
-        color_temp['colorCode'] = [255, 255, 255]
-        color_temp['colorFloat'] = [1.0, 1.0, 1.0]
-        color_temp['colorName'] = "New color"
-        return {'FINISHED'}
+        color_temp = getattr(bpy.context.window_manager, "ld_ColorPalette_temp")[0]
+        color_temp["color_code"] = [255, 255, 255]
+        color_temp["color_float"] = [1.0, 1.0, 1.0]
+        color_temp["color_name"] = "New color"
+        return {"FINISHED"}
 
-class ld_colorDeleteOperator(AsyncOperator):
+
+class ColorDeleteOperator(AsyncOperator):
     bl_idname = "object.ld_color_delete"
     bl_label = "Delete"
-    deleting_index: bpy.props.IntProperty() # type: ignore
-    
+    deleting_index: bpy.props.IntProperty()  # type: ignore
+
     async def async_execute(self, context: bpy.types.Context):
         print("deleting")
-        color_delete = getattr(context.window_manager,"ld_ColorPalette")[self.deleting_index]
-        res = await handle_colorDelete(int(color_delete['colorId']))
+        color_delete = getattr(context.window_manager, "ld_ColorPalette")[
+            self.deleting_index
+        ]
+        res = await handle_colorDelete(int(color_delete["color_id"]))
         if res:
             try:
-                self.report({'INFO'}, f"deleted color")
+                self.report({"INFO"}, f"deleted color")
             except:
                 print(res)
-        refreshColormap()
-        return {'FINISHED'}
-    
-class ld_colorCancelOperator(bpy.types.Operator):
+        refresh_colormap()
+        return {"FINISHED"}
+
+
+class ColorCancelOperator(bpy.types.Operator):
     bl_idname = "object.ld_color_cancel"
     bl_label = "Cancel"
-    
+
     @classmethod
     def poll(cls, context):
         return context.active_object is not None
-    
+
     def execute(self, context):
         handle_colorCancel(context)
-        panel = getattr(bpy.types,"LD_PT_color_palette")
+        panel = getattr(bpy.types, "LD_PT_color_palette")
         panel.editing_mode = False
-        refreshColormap()
-        return {'FINISHED'}
+        refresh_colormap()
+        return {"FINISHED"}
 
-class ld_colorConfirmOperator(AsyncOperator):
+
+class ColorConfirmOperator(AsyncOperator):
     bl_idname = "object.ld_color_confirm"
     bl_label = "Confirm"
     state = ""
+
     # TODO: types
     async def async_execute(self, context: bpy.types.Context):
-        panel = getattr(bpy.types,"LD_PT_color_palette")
+        panel = getattr(bpy.types, "LD_PT_color_palette")
         editing_state = panel.editing_state
         panel.editing_mode = False
         res = await handle_colorConfirm(editing_state)
         if res:
             try:
                 if editing_state == "EDIT":
-                    self.report({'INFO'}, f"edited color \"{res['color']}\"") # type: ignore
+                    self.report({"INFO"}, f"edited color \"{res['color']}\"")  # type: ignore
                 elif editing_state == "NEW":
-                    self.report({'INFO'}, f"added color \"{res['color']}\"") # type: ignore
+                    self.report({"INFO"}, f"added color \"{res['color']}\"")  # type: ignore
             except:
                 print(res)
-        refreshColormap()
-        return {'FINISHED'}
+        refresh_colormap()
+        return {"FINISHED"}
 
-operator_list = [ld_colorEditOperator, ld_colorDeleteOperator, ld_colorCancelOperator, ld_colorConfirmOperator, ld_colorNewOperator]
+
+operator_list = [
+    ColorEditOperator,
+    ColorDeleteOperator,
+    ColorCancelOperator,
+    ColorConfirmOperator,
+    ColorNewOperator,
+]
+
 
 def register():
     for op in operator_list:
         bpy.utils.register_class(op)
+
 
 def unregister():
     for op in operator_list:
--- editor-blender/panels/color_palette/__init__.py
+++ editor-blender/panels/color_palette/__init__.py
@@ -1,9 +1,11 @@
 import bpy
 from bpy.types import Context
-from gql import gql, Client
+from gql import Client, gql
 from gql.transport.aiohttp import AIOHTTPTransport
+
 from ...core.states import state
-#from Gql_utils import add_color, delete_color
+
+# from Gql_utils import add_color, delete_color
 # from ops import ld_colorEditOperator, ld_colorDeleteOperator, ld_colorCancelOperator, ld_colorConfirmOperator, ld_colorNewOperator
 
 """
@@ -23,82 +25,89 @@ query = gql(
     """
 )
 
-colormap = client.execute(query)['colorMap']['colorMap'] # to be fetched from state
+colormap = client.execute(query)["colorMap"]["colorMap"]  # to be fetched from state
 
 
 ## Init color collection
 
+
 def lock_colorFloat_change(item):
-    item.colorFloat = [x/255 for x in item.colorCode]
+    item.color_float = [x / 255 for x in item.color_code]
     # print("lock")
+
 
 def setup_color_data_from_state(colormap):
     for key in colormap:
-            item = getattr(bpy.context.window_manager,"ld_ColorPalette").add()
-            item.colorId = key
-            item.colorName = colormap[key]['color']
-            item.colorCode = colormap[key]['colorCode']
-            item.colorFloat = [x/255 for x in item.colorCode]
-            item.colorAlpha = 1.0
-            bpy.msgbus.subscribe_rna(
-                key=(item.colorFloat),
-                owner=bpy,
-                args=tuple([item]),
-                notify=lock_colorFloat_change
-                )
-            bpy.msgbus.subscribe_rna(
-                key=(item.colorCode),
-                owner=bpy,
-                args=tuple([item]),
-                notify=lock_colorFloat_change
-                )
-    color_temp = getattr(bpy.context.window_manager,"ld_ColorPalette_temp").add()
-    color_temp.colorCode = [0,0,0]
-    color_temp.colorFloat = [x/255 for x in color_temp.colorCode]
-    color_temp.colorName = ""
-    color_temp_item = getattr(bpy.context.window_manager,"ld_ColorPalette_temp")[0]
-    bpy.msgbus.subscribe_rna(
-            key=(color_temp_item.colorFloat),
+        item = getattr(bpy.context.window_manager, "ld_ColorPalette").add()
+        item.color_id = key
+        item.color_name = colormap[key]["color"]
+        item.color_code = colormap[key]["colorCode"]
+        item.color_float = [x / 255 for x in item.color_code]
+        item.color_alpha = 1.0
+        bpy.msgbus.subscribe_rna(
+            key=(item.color_float),
             owner=bpy,
-            args=tuple([color_temp_item]),
-            notify=lock_colorFloat_change
-            )
-    bpy.msgbus.subscribe_rna(
-            key=(color_temp_item.colorCode),
+            args=tuple([item]),
+            notify=lock_colorFloat_change,
+        )
+        bpy.msgbus.subscribe_rna(
+            key=(item.color_code),
             owner=bpy,
-            args=tuple([color_temp_item]),
-            notify=lock_colorFloat_change
-            )
+            args=tuple([item]),
+            notify=lock_colorFloat_change,
+        )
+    color_temp = getattr(bpy.context.window_manager, "ld_ColorPalette_temp").add()
+    color_temp.color_code = [0, 0, 0]
+    color_temp.color_float = [x / 255 for x in color_temp.color_code]
+    color_temp.color_name = ""
+    color_temp_item = getattr(bpy.context.window_manager, "ld_ColorPalette_temp")[0]
+    bpy.msgbus.subscribe_rna(
+        key=(color_temp_item.color_float),
+        owner=bpy,
+        args=tuple([color_temp_item]),
+        notify=lock_colorFloat_change,
+    )
+    bpy.msgbus.subscribe_rna(
+        key=(color_temp_item.color_code),
+        owner=bpy,
+        args=tuple([color_temp_item]),
+        notify=lock_colorFloat_change,
+    )
 
 
 ## Define handlers
 print("executing panel.py")
 
+
 class ColorPalettePanel(bpy.types.Panel):
     bl_label = "Colors"
     bl_idname = "LD_PT_color_palette"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
     bl_category = "ColorPalette"
-    
+
     editing_mode = False
-    editing_index = 0 # modified by edit operator
-    editing_state = "" # EDIT or NEW
-    
+    editing_index = 0  # modified by edit operator
+    editing_state = ""  # EDIT or NEW
+
     @classmethod
     def poll(cls, context: Context) -> bool:
         return state.is_logged_in
-    
+
     def draw(self, context):
         layout = self.layout
         layout.use_property_split = False
-        layout.use_property_decorate= False
-        
+        layout.use_property_decorate = False
+
         if not self.editing_mode:
-            for i in range(len(getattr(context.window_manager,"ld_ColorPalette"))):
-                item = getattr(context.window_manager,"ld_ColorPalette")[i]
+            for i in range(len(getattr(context.window_manager, "ld_ColorPalette"))):
+                item = getattr(context.window_manager, "ld_ColorPalette")[i]
                 row = layout.row()
-                row.prop(item,"colorFloat",text=f"[{item['colorId']}] {item['colorName']}")
+                row.prop(
+                    item,
+                    "color_float",
+                    text=f"[{item['color_id']}] {item['color_name']}",
+                )
                 row = layout.row()
                 op = row.operator("object.ld_color_edit")
                 setattr(op, "editing_index", i)
@@ -107,24 +116,24 @@ class ColorPalettePanel(bpy.types.Panel):
             row = layout.row()
             row.operator("object.ld_color_new")
         else:
-            temp_item = getattr(bpy.context.window_manager,"ld_ColorPalette_temp")[0]
+            temp_item = getattr(bpy.context.window_manager, "ld_ColorPalette_temp")[0]
             row = layout.row()
             row.label(text="[Edit mode]")
             row = layout.row()
-            row.prop(temp_item, 'colorName', text="Name")
+            row.prop(temp_item, "color_name", text="Name")
             row = layout.row()
-            row.prop(temp_item, 'colorCode', text="RGB")
+            row.prop(temp_item, "color_code", text="RGB")
             row = layout.row()
-            row.prop(temp_item, 'colorFloat', text="display")
+            row.prop(temp_item, "color_float", text="display")
             row = layout.row()
             row.operator("object.ld_color_cancel")
             row.operator("object.ld_color_confirm")
 
 
-
 def register():
     setup_color_data_from_state(colormap)
     bpy.utils.register_class(ColorPalettePanel)
+
 
 def unregister():
     bpy.utils.unregister_class(ColorPalettePanel)
