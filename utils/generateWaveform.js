5c8661eb5e5f5a689c599f366531b88a3f58e890
betty <bettycheng0921@gmail.com>
2024-01-17 00:07:20 +0800

    ugly but work

--- utils/generateWaveform.js
+++ utils/generateWaveform.js
@@ -5,6 +5,9 @@ const axios = require("axios");
 
 const child_process = require("child_process");
 
+const sharp = require('sharp');
+const jm = require('join-images');
+
 // the annotation part can be used when the file is a module
 
 // import { createWriteStream, existsSync, unlink } from "fs";
@@ -23,33 +26,41 @@ const child_process = require("child_process");
 // procedure: check existance->download->construct command->generate waveform
 
 // const img_res = '4000x1000';
-const img_res = '420000x1000';
+const img_res_width = 42000;
+const img_res_height = 4000;
+const img_res = String(img_res_width) + 'x' + String(img_res_height);
+console.log(`img_res = ${img_res}`);
 const hex_colo = '3D82B1';
 const sname = '../files/music/waveform.png';
 const musicName = '../files/music/2023.mp3';    // you can alter the music name here
 let ifp = path.join(String(__dirname), sname);       // name (path included) to the waveform image
 let sfp = path.join(String(__dirname), musicName);   // name (path included) to the music
+const leftName = '../files/music/leftWaveform.png';
+const rightName = '../files/music/rightWaveform.png';
+const lcp = path.join(String(__dirname), leftName);
+const rcp = path.join(String(__dirname), rightName);
+// console.log(`lcp = ${lcp}`);
 // console.log(`ifp = ${ifp}`);
 // console.log(`sfp = ${sfp}`);
 let releaseUrl = '';    // url for downloading ffmpeg
 let ffbin = '';   // the path name(file name included) of the ffmpeg binary
 let cmd = [];   // the command that will later be used to generate waveform
 
+
 const callFFMPEG = () => {
   cmd = ['sudo', 'ffmpeg'];
   cmd = [...cmd, '-i', String(sfp),
     '-hide_banner', '-loglevel', 'error', '-filter_complex',
-  `[0:a]aformat=channel_layouts=mono,showwavespic=s=${img_res}:colors=${hex_colo}:draw=full,crop=iw:ih/2:0:0`,
-    '-frames:v', '1',
-    '-y', String(ifp)]
+  `showwavespic=s=${img_res}:split_channels=1:colors=${hex_colo}`,
+    '-frames:v', '1', '-y', String(ifp)]
 
   let cmdString = cmd.join(' ');
   // console.log(`cmd = ${cmd.join(' ')}`);
   let startGenerationTime = Date.now();
   console.log("start generating");
-  try{
+  try {
     const ret = child_process.execSync(cmdString);
-  }catch(err){
+  } catch (err) {
     console.error('--- problem generating sound wave image');
     console.error(err);
   }
@@ -58,10 +69,10 @@ const callFFMPEG = () => {
 }
 
 // download from url and save as dest, then generate waveform
-const dl_url = (url, dest) => {
+const dl_url = async (url, dest) => {
   let start_time = Date.now();
   console.log("downloading ffmpeg...");
-  axios({
+  await axios({
     method: 'get',
     url: url,
     responseType: 'stream',
@@ -71,7 +82,7 @@ const dl_url = (url, dest) => {
   }).then((response) => {
     const writer = fs.createWriteStream(dest);
     response.data.pipe(writer);
-
+    // console.log("here1");
     return new Promise((resolve, reject) => {
       let error = null;
       writer.on('error', err => {
@@ -89,6 +100,7 @@ const dl_url = (url, dest) => {
       });
 
     });
+
   }).then(() => {
     const downloadTime = (Date.now() - start_time) / 1000;  // Convert milliseconds to seconds
     console.log(`Download time ${downloadTime.toFixed(2)}s`);
@@ -152,6 +164,79 @@ const ffmpegExist = () => {
   return fs.existsSync(ffbin);
 }
 
+const cutImages = () => {
+  sharp(String(ifp))
+    .extract({ left: 0, top: 0, width: img_res_width, height: img_res_height / 4 })
+    .toFile(String(lcp))
+    .then(() => {
+      sharp(String(ifp))
+        .extract({ left: 0, top: 3 * img_res_height / 4, width: img_res_width, height: img_res_height / 4 })
+        .toFile(String(rcp))
+        .then(() => {
+          jm.joinImages([String(lcp), String(rcp)], { direction: "vertical" })
+            .then((img) => {
+              // Save image as file
+              img.toFile(String(ifp));
+            }).catch((err) => {
+              console.log("Failed joining waveforms...")
+              console.log(err);
+            });
+        })
+    }).catch((err) => {
+      if (err) {
+        console.log("Failed extracting upper half of the waveform...")
+        console.log(err);
+      }
+    })
+  // console.log(`String(rcp) = ${String(rcp)}`);
+}
+
+const imageManipulate = async () => {
+  // console.log("here2");
+  sharp(String(ifp))
+    .extract({ left: 0, top: 0, width: img_res_width, height: img_res_height / 4 })
+    .toFile(String(lcp))
+    .then(() => {
+      sharp(String(ifp))
+        .extract({ left: 0, top: 3 * img_res_height / 4, width: img_res_width, height: img_res_height / 4 })
+        .toFile(String(rcp))
+        .then(() => {
+          jm.joinImages([String(lcp), String(rcp)], { direction: "vertical" })
+            .then((img) => {
+              // Save image as file
+              img.toFile(String(ifp));
+            }).catch((err) => {
+              console.log("Failed joining waveforms...")
+              console.log(err);
+            });
+        })
+    }).catch((err) => {
+      if (err) {
+        console.log("Failed extracting upper half of the waveform...")
+        console.log(err);
+      }
+    })
+
+
+}
+
+const mainGenerate = async (releaseUrl, ffbin) => {
+  await dl_url(releaseUrl, String(ffbin));
+  imageManipulate();
+}
+
+const rmRedundantWaveform = () => {
+  let rmCmd = [rm, String(lcp), String(rcp)];
+  rmCmdString = rmCmd.join(' ');
+  console.log("removing redundant waveforms...")
+  try {
+    const ret = child_process.execSync(rmCmdString);
+  } catch (err) {
+    console.error('--- problem removing redundant waveforms...');
+    console.error(err);
+  }
+}
+
 // download if ffmpeg not in path
 let ffmpegExistance = ffmpegExist();  // ffbin, releaseUrl is set in ffmpegExist
 // delete ffmpeg if exists
@@ -167,5 +252,4 @@ if (ffmpegExistance) {
   }
 }
 
-dl_url(releaseUrl, String(ffbin));
-
+mainGenerate(releaseUrl, ffbin);
