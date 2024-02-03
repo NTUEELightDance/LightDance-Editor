720e9b01c8854de5368fccbbcc1fa55ff9564fb4
張均豪 <62426283+jass921026@users.noreply.github.com>
2024-02-04 01:29:01 +0800

    EDITOR-#473 Rewrite UploadData (#489)
    * temp
    
    * a
    
    * debug
    
    * handle no effect

--- editor-server/src/routes/api/mod.rs
+++ editor-server/src/routes/api/mod.rs
@@ -32,4 +32,5 @@ pub fn build_api_routes() -> Router {
             "/getDancerLEDData",
             get(get_dancer_led_data::get_dancer_led_data),
         )
+        .route("/uploadData", post(upload_data::upload_data))
 }
