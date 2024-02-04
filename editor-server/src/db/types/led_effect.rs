5c087ee974ea62613ad65692b814dfaf49501dd8
tom1484 <tomchen2003611@gmail.com>
2024-02-05 00:41:15 +0800

    attach led effects to models and allow model used by multiple dancers

--- editor-server/src/db/types/led_effect.rs
+++ editor-server/src/db/types/led_effect.rs
@@ -8,10 +8,6 @@ use serde::{Deserialize, Serialize};
 pub struct LEDEffectData {
     pub id: i32,
     pub name: String,
-<<<<<<< HEAD
     pub model_id: i32,
-=======
-    pub dancer_id: i32,
->>>>>>> 1424e6b (add dancer layer to LED map)
     pub part_id: i32,
 }
