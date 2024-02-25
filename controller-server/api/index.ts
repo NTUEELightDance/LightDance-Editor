dee294d46a41af07ea9265fdcb13f79351429c65
lightdance <ntuee@ntu.edu.tw>
2024-02-25 17:44:44 +0800

    modify pinMapTable part

--- controller-server/api/index.ts
+++ controller-server/api/index.ts
@@ -1,10 +1,13 @@
 import { OF, LED, OFSchema, LEDSchema } from "@/schema/global";
 
+import pinMapTable from "@/configs/pinMapTable";
+
 import { instance } from "./axios";
 
-export async function getDancerLEDDataAPI(dancer: string) {
+export async function getDancerLEDDataAPI(RPiName: string) {
+  const getRPiDataJSON = pinMapTable[RPiName];
   const { data } = await instance.get<LED>("/getDancerLEDData", {
-    params: { dancer },
+    params: { getRPiDataJSON },
   });
 
   LEDSchema.parse(data);
@@ -12,9 +15,10 @@ export async function getDancerLEDDataAPI(dancer: string) {
   return data;
 }
 
-export async function getDancerFiberDataAPI(dancer: string) {
+export async function getDancerFiberDataAPI(RPiName: string) {
+  const getRPiDataJSON = pinMapTable[RPiName];
   const { data } = await instance.get<OF>("/getDancerFiberData", {
-    params: { dancer },
+    params: { getRPiDataJSON },
   });
 
   OFSchema.parse(data);
--- controller-server/schema/PinMapTable.ts
+++ controller-server/schema/PinMapTable.ts
@@ -1,15 +1,19 @@
 import { z } from "zod";
 
 export const PinMapSchema = z.object({
+  dancer: z.string(),
   fps: z.number(),
   OFPARTS: z.record(z.string(), z.number()),
-  LEDPARTS: z.record(
-    z.string(),
-    z.object({
-      id: z.number(),
-      len: z.number(),
-    })
-  ),
+  // LEDPARTS: z.record(
+  //   z.string(),
+  //   z.object({
+  //     id: z.number(),
+  //     len: z.number(),
+  //   })
+  // ),
+  // LEDPARTS_MERGE: z.record(
+  //   z.array(z.string()),
+  // ),
 });
 
 export const PinMapTableSchema = z.record(z.string(), PinMapSchema);
