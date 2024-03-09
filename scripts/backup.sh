f0b18359999eaea813bb3fccb8b4a923966f53f9
tom1484 <tomchen2003611@gmail.com>
2024-03-10 02:03:55 +0800

    hot fix: add crontab example

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,6 +1,10 @@
 # Usage: ./backup.sh <editorPath> <backupPath>
 # Description: This script is used to backup the data of the editor to the backupPath
 # Example: ./backup.sh ~/LightDancer-Editor ~/LightTableBackup
+# Note: You can setup crontab to run this script periodically
+#      1. crontab -e
+#      2. Add a line like this: 0 * * * * sh ~/LightDance-Editor/scripts/backup.sh ~/LightDance-Editor ~/LightTableBackup
+#      3. Save and exit
 
 editorPath=$1
 backupPath=$2
