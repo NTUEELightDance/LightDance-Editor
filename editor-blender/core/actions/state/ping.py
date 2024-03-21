e4fe76d9ea3e19e27e6e934d63432fd836a6da37
tom1484 <tomchen2003611@gmail.com>
2024-03-22 01:30:58 +0800

    Add fail count to ping

--- editor-blender/core/actions/state/ping.py
+++ editor-blender/core/actions/state/ping.py
@@ -1,7 +1,16 @@
 from ....api.ping_agent import ping_agent
 from .app_state import set_sync
 
+fail_count = 0
+
 
 async def ping_server():
     pinged = await ping_agent.ping()
-    set_sync(pinged)
+    if not pinged:
+        global fail_count
+        fail_count += 1
+        if fail_count >= 3:
+            set_sync(False)
+    else:
+        fail_count = 0
+        set_sync(True)
