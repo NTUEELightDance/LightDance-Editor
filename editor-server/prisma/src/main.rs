904e779ea3cb418a2d3d36cbacbc5bf7159ab879
durant42040 <b11901040@ntu.edu.tw>
2025-01-09 22:44:49 +0800

    bug fix

--- editor-server/prisma/src/main.rs
+++ editor-server/prisma/src/main.rs
@@ -1,14 +1,14 @@
-use std::env;
+use std::env::{var, args};
 use std::time;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = args().collect();
 
     // Check MySQL connection when migrating
     if args.len() >= 2 && args[1] == "migrate" {
         dotenv::dotenv().ok();
 
-        let mysql_host = env!("DATABASE_URL", "DATABASE_URL is not set");
+        let mysql_host = var("DATABASE_URL").expect("DATABASE_URL is not set");
         println!("MySQL Host: {}", mysql_host);
 
         while let Err(_) = mysql::Conn::new(mysql_host.as_str()) {
