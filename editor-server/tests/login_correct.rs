42b1ab76856a3024ec61881f17aee117e06309a2
johowjo <johowjo@gmail.com>
2025-01-26 17:59:37 +0800

    slight modification to login tests

--- editor-server/tests/login_correct.rs
+++ editor-server/tests/login_correct.rs
@@ -5,18 +5,24 @@ mod correct_login_test {
         body::Body,
         http::{Request, StatusCode},
     };
-    use editor_server::build_app;
+    use editor_server::{build_app, global};
     use std::env::var;
     use tower::{Service, ServiceExt};
 
     #[tokio::test]
     async fn login_correct() {
+        // this test is only nontrivial when run in production mode
         // in order to run this test in production mode, AUTH0_TEST_USERNAME and
         // AUTH0_TEST_PASSWORD must be set in .env
-        //
+
         dotenv::dotenv().ok();
 
         let mut app = build_app().await;
+
+        let env_type = &global::envs::get().env;
+        if env_type == "development" {
+            return;
+        }
 
         let username = var("AUTH0_TEST_USERNAME").expect("test username not set");
         let password = var("AUTH0_TEST_PASSWORD").expect("test password not set");
