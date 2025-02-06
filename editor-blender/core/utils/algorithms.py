feb34a6f6abd6f34563e5e0183f921ce4c4b5b9a
ExuberantIris <imanthony1010@gmail.com>
2025-02-06 19:43:27 +0800

    EDITOR-#620 Fix lint issues.

--- editor-blender/core/utils/algorithms.py
+++ editor-blender/core/utils/algorithms.py
@@ -29,16 +29,15 @@ def _binary_search_with_lr(
     """
     :param arr: sorted list of integers
     :param x: integer to search for
-    :return: (index of the first number in arr that is larger than or equal to x,
-              index of the last number in arr that is less than or equal to x)
-              or out of range error: showing that the number is bigger/smaller than all num. in array
+    :return: (index of the last number in arr that is less than or equal to x,
+              index of the first number in arr that is larger than or equal to x)
+             or tuple of 'out of range' error: showing that the number is bigger/smaller than all num. in array
     """
     l, r = 0, len(arr) - 1
     if x > arr[r]:
         return ("OutOfRange_Larger", "OutOfRange_Larger")
     elif x < arr[l]:
         return ("OutOfRange_Smaller", "OutOfRange_Smaller")
-
     while r - l > 1 or len(arr) == 2:
         mid = (r + l) // 2
         if arr[mid] < x:
@@ -49,15 +48,17 @@ def _binary_search_with_lr(
             l = mid
             r = mid
             break
-
         if len(arr) == 2:
             break
-
     return (l, r)
 
 
-# find the smallest cont. range in arr that include [left, right] which is in the form of indexes
 def binary_search_for_range(arr: list[int], left: int, right: int) -> tuple[int, int]:
+    """
+    :param arr: sorted list of integers
+    :param left, right: designated range
+    :return: the smallest continuous range in arr that includes [left, right], which is in the form of indexes
+    """
     search_l = _binary_search_with_lr(arr, left)[0]
     search_r = _binary_search_with_lr(arr, right)[1]
 
@@ -65,10 +66,8 @@ def binary_search_for_range(arr: list[int], left: int, right: int) -> tuple[int,
         return (len(arr) - 1, len(arr) - 1)
     if search_r == "OutOfRange_Smaller":
         return (0, 0)
-
     if search_l == "OutOfRange_Smaller":
         search_l = 0
     if search_r == "OutOfRange_Larger":
         search_r = len(arr) - 1
-
     return (search_l, search_r)
