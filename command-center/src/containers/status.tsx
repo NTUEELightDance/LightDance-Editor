d035a06cb8755ecce634a0de61f72fc64a706fb4
Chalkman071 <josephtseng2004@gmail.com>
2025-02-07 16:22:35 +0800

    Generate fake dancer data

--- command-center/src/containers/status.tsx
+++ command-center/src/containers/status.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { Text, Box } from "ink";
 import chalk from "chalk";
+import { useDancer } from "../hooks/useDancer.js";
 
 export default function StatusPanel() {
+  const { dancers } = useDancer();
+  console.log(dancers);
   return (
     <Box>
       <Text>{chalk.cyan("Status Panel")}</Text>
