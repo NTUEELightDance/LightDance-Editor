b5d71a7c351a0b7aa288f2599a32478e4b3b9893
durant42040 <b11901040@ntu.edu.tw>
2025-03-14 15:23:38 +0800

    bug fix

--- utils/changeLEDAlpha.js
+++ utils/changeLEDAlpha.js
@@ -3,13 +3,13 @@ const path = require('path');
 
 const data = require("./jsons/exportDataEmpty.json");
 
-for (let i = 1; i < Object.keys(data.control).length + 1; i++) {
-    let status = data.control[i].status;
+for (const [_, controlData] of Object.entries(data.control)) {
+    let status = controlData.status;
     for (let j = 0; j < status.length; j++) {
         let parts = data.dancer[j].parts;
         for (let k = 0; k < parts.length; k++) {
             if (parts[k].type === "LED") {
-                data.control[i].status[j][k][1] = 150;
+                controlData.status[j][k][1] = 150;
             }
         }
     }
