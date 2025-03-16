7bb56d57703723bf86f6a7f93e6df2554b246b8c
durant42040 <b11901040@ntu.edu.tw>
2025-03-16 17:14:38 +0800

    fix bug

--- utils/createSpinningLED.js
+++ utils/createSpinningLED.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const data = require("./jsons/exportDataEmpty.json");
 
-const startTime = 6000;
+const startTime = 2000;
 const endTime = 16000;
 const period = 1000;
 const LEDlength = 6;
@@ -11,14 +11,15 @@ const PropName = "11_small_orb_1";
 const LEDPart = "main_LED";
 const defaultColorData = ["blue", 255];
 const secondaryColorData = ["red", 255];
+const direction = 1;
 
 const partLength = data.dancer.find(d => d.name === PropName).parts.find(d => d.name === LEDPart).length;
 const index = data.dancer.findIndex(d => d.name === PropName);
 const LEDindex = data.dancer[index].parts.findIndex(d => d.name === LEDPart);
 
-const addFrame = (t) => {
+const addFrame = (start) => {
     const previousControl = Object.values(data.control)
-        .filter(d => d.start <= t)
+        .filter(d => d.start <= start)
         .reduce((prev, curr) => (curr.start > prev.start ? curr : prev), { start: -Infinity });
     const status = JSON.parse(JSON.stringify(previousControl.status)); 
     const led_status = JSON.parse(JSON.stringify(previousControl.led_status)); 
@@ -26,16 +27,23 @@ const addFrame = (t) => {
     status[index][LEDindex] = ["", 150];
     led_status[index][LEDindex] = Array(partLength).fill(defaultColorData);
 
-    for (let i = (t - startTime) * partLength / period; i < (t - startTime) * partLength / period + LEDlength; i++) {
-        led_status[index][LEDindex][Math.round(i) % partLength] = secondaryColorData;
+    for (let i = (start - startTime) * partLength / period; i < (start - startTime) * partLength / period + LEDlength; i++) {
+        led_status[index][LEDindex][((direction * Math.round(i)) % partLength + partLength) % partLength] = secondaryColorData;
     }
     
     const controlData = {
-        start: t,
-        fade: false,
+        start,
+        fade: previousControl.fade,
         status,
         led_status,
     };
+
+    const entry = Object.entries(data.control).find(([_, value]) => value.start == start);
+    if (entry) {
+        const [key, _] = entry;
+        data.control[key] = controlData;
+        return;
+    }
 
     const maxKey = Math.max(...Object.keys(data.control).map(Number));
     const nextKey = maxKey + 1;
