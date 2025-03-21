7c64b369066a546e5e85cb94011e877595c151e8
durant42040 <b11901040@ntu.edu.tw>
2025-03-21 14:32:11 +0800

    update props scripts

---
+++ utils/changeDancerColor.js
@@ -0,0 +1,37 @@
+
+
+
+const data = require("../../LightTableBackup/2025.03.19.json");
+const fs = require('fs');
+const path = require('path');
+
+for (const [key, controlData] of Object.entries(data.control)) {
+    for (let part_id = 0; part_id < 31; part_id++) {
+        if (controlData.status[0][part_id][0] === "bad_blue" || controlData.status[0][part_id][0] === "bad_green" || controlData.status[0][part_id][0] === "bad_orange_light_p" || controlData.status[0][part_id][0] === "orange_dark_p") {
+            data.control[key].status[0][part_id][0] = "bad_orange_crayola";
+        }
+    }
+    for (let dancer_id = 1; dancer_id < 5; dancer_id++) {
+        for (let part_id = 0; part_id < 39; part_id++) {
+            if (controlData.status[dancer_id][part_id][0] === "bad_blue" || controlData.status[dancer_id][part_id][0] === "bad_green" || controlData.status[dancer_id][part_id][0] === "bad_orange_light_p" || controlData.status[dancer_id][part_id][0] === "orange_dark_p") {
+                data.control[key].status[dancer_id][part_id][0] = "bad_orange_crayola";
+            }
+            if (controlData.status[dancer_id][part_id][0] === "bad_blur_blue") {
+                data.control[key].status[dancer_id][part_id][0] = "bad_red";
+            }
+            if (controlData.status[dancer_id][35][0] === "bad_orange_crayola") {
+                data.control[key].status[dancer_id][35][0] = "bad_red";
+            }
+        }
+    }
+    for (let dancer_id = 5; dancer_id < 8; dancer_id++) {
+        for (let part_id = 0; part_id < 36; part_id++) {
+            if (controlData.status[dancer_id][part_id][0] === "pink") {
+                data.control[key].status[dancer_id][part_id][0] = "good_Purple";
+            }
+        }
+    }
+}
+
+fs.writeFileSync(path.join(__dirname, "./updatedColor.json"), JSON.stringify(data, null, 2));
+console.log("Updated data has been saved to ./updatedColor.json");
--- utils/createBigCannon.js
+++ utils/createBigCannon.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-// const data = require("./../../LightTableBackup/2025.03.17.json");
+// const data = require("./../../LightTableBackup/2025.03.20.json");
 const data = require("./jsons/exportDataEmpty.json");
-const { dir } = require('console');
 
 const addFrame = (start) => {
     const previousControl = Object.values(data.control)
@@ -31,10 +30,11 @@ const addFrame = (start) => {
     data.control[nextKey.toString()] = controlData;
 }
 
-const updateFrame = (key, frame, defaultColorData, secondaryColorData, direction, double, partLength) => {
+const updateFrame = (key, frame, defaultColorData, secondaryColorData, direction, double, partLength, startTime, endTime, startPos, endPos) => {
     const start = frame.start;
     const status = JSON.parse(JSON.stringify(frame.status)); 
     const led_status = JSON.parse(JSON.stringify(frame.led_status)); 
+    const center = startPos + Math.round((start - startTime) / (endTime - startTime) * (endPos - startPos));
 
     status[24][0] = ["black", 150];
     // mid
@@ -52,14 +52,14 @@ const updateFrame = (key, frame, defaultColorData, secondaryColorData, direction
     status[25][5] = ["black", 150];
     status[25][6] = ["black", 150];
 
-    led_status[24][1] = Array(92).fill(defaultColorData);
-    led_status[24][2] = Array(41).fill(defaultColorData);
-    led_status[25][0] = Array(30).fill(defaultColorData);
-    led_status[25][2] = Array(30).fill(defaultColorData);
-    led_status[25][3] = Array(24).fill(defaultColorData);
-    led_status[25][5] = Array(24).fill(defaultColorData);
-    led_status[25][1] = Array(41).fill(defaultColorData);
-    led_status[25][4] = Array(92).fill(defaultColorData);
+    led_status[24][1] = Array(92).fill(defaultColorDataRight);
+    led_status[24][2] = Array(41).fill(defaultColorDataRight);
+    led_status[25][0] = Array(30).fill(defaultColorDataRight);
+    led_status[25][2] = Array(30).fill(defaultColorDataRight);
+    led_status[25][3] = Array(24).fill(defaultColorDataRight);
+    led_status[25][5] = Array(24).fill(defaultColorDataRight);
+    led_status[25][1] = Array(41).fill(defaultColorDataRight);
+    led_status[25][4] = Array(92).fill(defaultColorDataRight);
 
     if (direction != 0) {
         for (let i = (start - startTime) * partLength / period; i < (start - startTime) * partLength / period + LEDlength; i++) {
@@ -71,7 +71,7 @@ const updateFrame = (key, frame, defaultColorData, secondaryColorData, direction
                 led_status[24][1][bulbIndex - 41] = secondaryColorData;
             }
             if (bulbIndex >= 133 && bulbIndex < 225) {
-                led_status[25][4][bulbIndex - 133] = secondaryColorData;
+                led_status[25][4][92 - bulbIndex + 133] = secondaryColorData;
             }
             if (bulbIndex >= 225 && bulbIndex < partLength) {
                 led_status[25][1][bulbIndex - 225] = secondaryColorData;
@@ -86,7 +86,7 @@ const updateFrame = (key, frame, defaultColorData, secondaryColorData, direction
                     led_status[24][1][bulbIndex - 41] = secondaryColorData;
                 }
                 if (bulbIndex >= 133 && bulbIndex < 225) {
-                    led_status[25][4][bulbIndex - 133] = secondaryColorData;
+                    led_status[25][4][92 - bulbIndex + 133] = secondaryColorData;
                 }
                 if (bulbIndex >= 225 && bulbIndex < partLength) {
                     led_status[25][1][bulbIndex - 225] = secondaryColorData;
@@ -95,25 +95,79 @@ const updateFrame = (key, frame, defaultColorData, secondaryColorData, direction
         }
     } else {
         // move to middle
-        for (let i = (start - startTime) * partLength / period; i < (start - startTime) * partLength / period + LEDlength; i++) {
-            const center = 50;
-            let bulb1 = ((Math.round(i)) % partLength + partLength) % partLength;
+        let partLength1 = center;
+        for (let i = 0; i < partLength; i++) {
+            if (i < center) {
+                if (i >= 0 && i < 41) {
+                    led_status[24][2][i] = defaultColorDataRight;
+                }
+                if (i >= 41 && i < 133) {
+                    led_status[24][1][i - 41] = defaultColorDataRight;
+                }
+                if (i >= 133 && i < 225) {
+                    led_status[25][4][92 - i + 133] = defaultColorDataRight;
+                }
+                if (i >= 225 && i < partLength) {
+                    led_status[25][1][i - 225] = defaultColorDataRight;
+                }
+            } else {
+                if (i >= 0 && i < 41) {
+                    led_status[24][2][i] = defaultColorDataLeft;
+                }
+                if (i >= 41 && i < 133) {
+                    led_status[24][1][i - 41] = defaultColorDataLeft;
+                }
+                if (i >= 133 && i < 225) {
+                    led_status[25][4][92 - i + 133] = defaultColorDataLeft;
+                }
+                if (i >= 225 && i < partLength) {
+                    led_status[25][1][i - 225] = defaultColorDataLeft;
+                }
+            }
+        }
+        for (let i = center - 10; i < center + 10; i++) {
+            if (i >= 0 && i < 41) {
+                led_status[24][2][41 - i] = pulseColorData;
+            }
+            if (i >= 41 && i < 133) {
+                led_status[24][1][i - 41] = pulseColorData;
+            }
+            if (i >= 133 && i < 225) {
+                led_status[25][4][92 - i + 133] = pulseColorData;
+            }
+            if (i >= 225 && i < partLength) {
+                led_status[25][1][i - 225] = pulseColorData;
+            }
+        }
+        for (let i = (start - startTime) * partLength1 / period; i < (start - startTime) * partLength1 / period + LEDlength; i++) {
+            let bulb1 = ((Math.round(i)) % partLength1 + partLength1) % partLength1
             if (bulb1 >= 0 && bulb1 < 41) {
-                led_status[24][2][41 - bulb1] = secondaryColorData;
-                led_status[25][1][41 - bulb1] = secondaryColorData;
+                led_status[24][2][41 - bulb1] = secondaryColorDataRight;
             }
             if (bulb1 >= 41 && bulb1 < 133) {
-                led_status[24][1][bulb1 - 41] = secondaryColorData;
-                led_status[25][4][92 - bulb1 + 41] = secondaryColorData;
+                led_status[24][1][bulb1 - 41] = secondaryColorDataRight;
             }
-            let bulb2 = ((Math.round(i)) % partLength + partLength) % partLength;
+            if (bulb1 >= 133 && bulb1 < 225) {
+                led_status[25][4][92 - bulb1 + 133] = secondaryColorDataRight;
+            }
+            if (bulb1 >= 225 && bulb1 < partLength) {
+                led_status[25][1][bulb1 - 225] = secondaryColorDataRight;
+            }
+        }
+        let partLength2 = partLength - center;
+        for (let i = (start - startTime) * partLength2 / period; i < (start - startTime) * partLength2 / period + LEDlength; i++) {
+            let bulb2 = ((-Math.round(i)) % partLength2 + partLength2) % partLength2 + center
             if (bulb2 >= 0 && bulb2 < 41) {
-                led_status[24][2][41 - bulb2] = secondaryColorData;
-                led_status[25][1][41 - bulb2] = secondaryColorData;
+                led_status[24][2][41 - bulb2] = secondaryColorDataLeft;
             }
             if (bulb2 >= 41 && bulb2 < 133) {
-                led_status[24][1][bulb2 - 41] = secondaryColorData;
-                led_status[25][4][92 - bulb2 + 41] = secondaryColorData;
+                led_status[24][1][bulb2 - 41] = secondaryColorDataLeft;
+            }
+            if (bulb2 >= 133 && bulb2 < 225) {
+                led_status[25][4][92 - bulb2 + 133] = secondaryColorDataLeft;
+            }
+            if (bulb2 >= 225 && bulb2 < partLength) {
+                led_status[25][1][bulb2 - 225] = secondaryColorDataLeft;
             }
         }
     }
@@ -147,8 +201,8 @@ const updateFrame = (key, frame, defaultColorData, secondaryColorData, direction
     data.control[key] = controlData;
 }
 
-const createBigCannon = (start, end, period, LEDlength, defaultColorData, secondaryColorData, direction, double, partLength) => {
-    for (let t = start; t < end; t += (period / partLength * LEDlength)) {
+const createBigCannon = (start, end, period, LEDlength, defaultColorData, secondaryColorData, direction, double, partLength, startPos, endPos) => {
+    for (let t = start; t < end; t += (period / partLength * LEDlength * 2)) {
         addFrame(Math.round(t));
     }
     if (direction === "right") {
@@ -161,53 +215,161 @@ const createBigCannon = (start, end, period, LEDlength, defaultColorData, second
 
     const existingFrames = Object.keys(data.control).filter((key) => data.control[key].start >= start && data.control[key].start <= end);
     existingFrames.forEach((key) => {
-        updateFrame(key, data.control[key], defaultColorData, secondaryColorData, direction, double, partLength);
+        updateFrame(key, data.control[key], defaultColorData, secondaryColorData, direction, double, partLength, startTime, endTime, startPos, endPos);
     });
 }
 
-let period = 1000;
+let period = 500;
 let LEDlength = 15;
 // left: 1. right: -1
-let direction = "right";
-let double = true;
+let direction = "mid";
 
 let partLength = 266;
 
-let startTime = 55000;
-let endTime =  70000;
-let defaultColorData = ["blue", 255];
-let secondaryColorData = ["white", 255];
+let startTime = 390007;
+let endTime =  391207;
 
-// createBigCannon(startTime, endTime, period, LEDlength, defaultColorData, secondaryColorData, direction, double, partLength);
+let defaultColorData = []
+let defaultColorDataRight = ["blue", 255];
+let defaultColorDataLeft = ["black", 255];
+let secondaryColorDataRight = ["light_blue", 255];
+let secondaryColorDataLeft = ["black", 255];
+let pulseColorData = ["light_blue", 255];
 
-period = 1000;
-LEDlength = 15;
-// left: 1. right: -1
-direction = "left";
-double = true;
+let secondaryColorData = []
+let double = false;
 
-partLength = 266;
-
-startTime = 40000;
-endTime =  55000;
-defaultColorData = ["blue", 255];
-secondaryColorData = ["white", 255];
-
-// createBigCannon(startTime, endTime, period, LEDlength, defaultColorData, secondaryColorData, direction, double, partLength);
+createBigCannon(startTime, endTime, period, LEDlength, defaultColorData, secondaryColorData, direction, secondaryColorData, partLength, 0, 266);
 
 period = 500;
 LEDlength = 15;
 // left: 1. right: -1
 direction = "mid";
 
-partLength = 133;
+partLength = 266;
 
-startTime = 25000;
-endTime =  40000;
-defaultColorData = ["blue", 255];
-secondaryColorData = ["white", 255];
+startTime = 391207;
+endTime =  393007;
 
-createBigCannon(startTime, endTime, period, LEDlength, defaultColorData, secondaryColorData, direction, double, partLength);
+defaultColorData = []
+defaultColorDataRight = ["blue", 255];
+defaultColorDataLeft = ["black", 255];
+secondaryColorDataRight = ["light_blue", 255];
+secondaryColorDataLeft = ["black", 255];
+pulseColorData = ["light_blue", 255];
+
+secondaryColorData = []
+double = false;
+
+createBigCannon(startTime, endTime, period, LEDlength, defaultColorData, secondaryColorData, direction, secondaryColorData, partLength, 266, 266);
+
+
+period = 500;
+LEDlength = 15;
+// left: 1. right: -1
+direction = "mid";
+
+partLength = 266;
+
+startTime = 394807;
+endTime =  396007;
+
+defaultColorData = []
+defaultColorDataRight = ["black", 255];
+defaultColorDataLeft = ["red", 255];
+secondaryColorDataRight = ["black", 255];
+secondaryColorDataLeft = ["light_red", 255];
+pulseColorData = ["light_red", 255];
+
+secondaryColorData = []
+double = false;
+
+createBigCannon(startTime, endTime, period, LEDlength, defaultColorData, secondaryColorData, direction, secondaryColorData, partLength, 266, 0);
+
+period = 500;
+LEDlength = 15;
+// left: 1. right: -1
+direction = "mid";
+
+partLength = 266;
+
+startTime = 396007;
+endTime =  397207;
+
+defaultColorData = []
+defaultColorDataRight = ["black", 255];
+defaultColorDataLeft = ["red", 255];
+secondaryColorDataRight = ["black", 255];
+secondaryColorDataLeft = ["light_red", 255];
+
+secondaryColorData = []
+double = false;
+
+createBigCannon(startTime, endTime, period, LEDlength, defaultColorData, secondaryColorData, direction, secondaryColorData, partLength, 0, 0);
+
+
+period = 500;
+LEDlength = 15;
+// left: 1. right: -1
+direction = "mid";
+
+partLength = 266;
+
+startTime = 405607;
+endTime =  406207;
+
+defaultColorData = []
+defaultColorDataRight = ["blue", 255];
+defaultColorDataLeft = ["red", 255];
+secondaryColorDataRight = ["light_blue", 255];
+secondaryColorDataLeft = ["light_red", 255];
+pulseColorData = ["white", 255];
+
+secondaryColorData = []
+double = false;
+
+createBigCannon(startTime, endTime, period, LEDlength, defaultColorData, secondaryColorData, direction, secondaryColorData, partLength, 133, 133);
+
+period = 500;
+LEDlength = 15;
+// left: 1. right: -1
+direction = "mid";
+
+partLength = 266;
+
+startTime = 406207;
+endTime =  407407;
+
+
+createBigCannon(startTime, endTime, period, LEDlength, defaultColorData, secondaryColorData, direction, double, partLength, 133, 133);
+
+period = 500;
+LEDlength = 15;
+// left: 1. right: -1
+direction = "mid";
+startTime = 407407;
+endTime =  408007;
+
+createBigCannon(startTime, endTime, period, LEDlength, defaultColorData, secondaryColorData, direction, double, partLength, 133, 133);
+
+period = 500;
+LEDlength = 15;
+// left: 1. right: -1
+direction = "mid";
+startTime = 408007;
+endTime =  409207;
+
+createBigCannon(startTime, endTime, period, LEDlength, defaultColorData, secondaryColorData, direction, double, partLength, 133, 133);
+
+period = 250;
+LEDlength = 15;
+// left: 1. right: -1
+direction = "mid";
+startTime = 409207;
+endTime =  410407;
+
+createBigCannon(startTime, endTime, period, LEDlength, defaultColorData, secondaryColorData, direction, double, partLength, 133, 133);
+
 
 // fs.writeFileSync(path.join(__dirname, "./../../LightTableBackup/2025.03.17.json"), JSON.stringify(data, null, 0));
 fs.writeFileSync(path.join(__dirname, "./props.json"), JSON.stringify(data, null, 2));
